ed653a104edd6b108a31f9cf00a614e314678f78
Kevin DeLoach <kdeloach@azavea.com>
2015-04-01 11:06:53 -0400

    Make bundle script accessible to ansible
    The bundle script needs to be accessible within the host VM in order
    for it to be executable by ansible.

--- scripts/bundle.sh
+++ scripts/bundle.sh
@@ -3,54 +3,8 @@
 # Bundle JS and CSS static assets.
 
 set -e
+set -x
 
-# Defaults
-ENTRY_JS_FILES="./js/src/main.js"
-STATIC_JS_DIR=/var/www/mmw/static/js
-BROWSERIFY=./node_modules/.bin/browserify
+ARGS=$*
 
-ENTRY_CSS_FILE=./sass/main.scss
-STATIC_CSS_DIR=/var/www/mmw/static/css
-NODESASS=./node_modules/.bin/node-sass
-
-usage() {
-    echo -n "$(basename $0) [OPTION]...
-
-Bundle JS and CSS static assets.
-
- Options:
-  --watch      Listen for file changes
-  --debug      Generate source maps
-  -h, --help   Display this help text
-"
-}
-
-# Read options
-while [[ $1 = -?* ]]; do
-  case $1 in
-    -h|--help) usage; exit 1 ;;
-    --watch) ENABLE_WATCH=1 ;;
-    --debug) ENABLE_DEBUG=1 ;;
-  esac
-  shift
-done
-
-if [ -n "$ENABLE_DEBUG" ]; then
-    BROWSERIFY="$BROWSERIFY --debug"
-    NODESASS="$NODESASS --source-map $STATIC_CSS_DIR/main.css.map --source-map-contents"
-fi
-
-if [ -n "$ENABLE_WATCH" ]; then
-    BROWSERIFY=./node_modules/.bin/watchify
-    # These flags have to appear at the end or watchify will exit.
-    EXTRA_ARGS="--verbose --poll"
-    NODESASS="$NODESASS --watch --recursive"
-fi
-
-VAGRANT_COMMAND="cd /opt/app && \
-    mkdir -p $STATIC_JS_DIR $STATIC_CSS_DIR && { \
-    $NODESASS $ENTRY_CSS_FILE -o $STATIC_CSS_DIR & \
-    $BROWSERIFY $ENTRY_JS_FILES -o $STATIC_JS_DIR/main.js $EXTRA_ARGS; }"
-
-echo "$VAGRANT_COMMAND"
-vagrant ssh app -c "$VAGRANT_COMMAND"
+vagrant ssh app -c "cd /opt/app && envdir /etc/mmw.d/env ./bundle.sh $ARGS"
