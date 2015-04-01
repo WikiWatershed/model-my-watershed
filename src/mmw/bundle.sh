d0c1c29e31a269444886048b239a20bfb9e9f02e
Kevin DeLoach <kdeloach@azavea.com>
2015-04-01 13:09:37 -0400

    Rename some local variables
    To increase clarity/readablilty.

--- src/mmw/bundle.sh
+++ src/mmw/bundle.sh
@@ -12,9 +12,9 @@ ENTRY_JS_FILES="./js/src/main.js"
 STATIC_JS_DIR="$DJANGO_STATIC_ROOT/js"
 BROWSERIFY=./node_modules/.bin/browserify
 
-ENTRY_CSS_FILE=./sass/main.scss
+ENTRY_SASS_FILE=./sass/main.scss
 STATIC_CSS_DIR="$DJANGO_STATIC_ROOT/css"
-NODESASS=./node_modules/.bin/node-sass
+NODE_SASS=./node_modules/.bin/node-sass
 
 usage() {
     echo -n "$(basename $0) [OPTION]...
@@ -42,17 +42,17 @@ if [ -n "$ENABLE_WATCH" ]; then
     BROWSERIFY=./node_modules/.bin/watchify
     # These flags have to appear at the end or watchify will exit.
     EXTRA_ARGS="--verbose --poll"
-    NODESASS="$NODESASS --watch --recursive"
+    NODE_SASS="$NODE_SASS --watch --recursive"
 fi
 
 if [ -n "$ENABLE_DEBUG" ]; then
     BROWSERIFY="$BROWSERIFY --debug"
-    NODESASS="$NODESASS --source-map $STATIC_CSS_DIR/main.css.map --source-map-contents"
+    NODE_SASS="$NODE_SASS --source-map $STATIC_CSS_DIR/main.css.map --source-map-contents"
 fi
 
 VAGRANT_COMMAND="cd /opt/app && \
     mkdir -p $STATIC_JS_DIR $STATIC_CSS_DIR && { \
-    $NODESASS $ENTRY_CSS_FILE -o $STATIC_CSS_DIR & \
+    $NODE_SASS $ENTRY_SASS_FILE -o $STATIC_CSS_DIR & \
     $BROWSERIFY $ENTRY_JS_FILES -o $STATIC_JS_DIR/main.js $EXTRA_ARGS; }"
 
 echo "$VAGRANT_COMMAND"
