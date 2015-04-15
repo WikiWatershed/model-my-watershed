7dfed7a5c738d2d640c4c50b4ed81e8dc3e8b75a
Casey Thomas <cthomas@azavea.com>
2015-04-15 09:45:22 -0400

    Add support for the Marionette Inspector.
    * The inspection needs access to Backbone and Marionette.
    
    Closes #45

--- src/mmw/js/shim/backbone.marionette.js
+++ src/mmw/js/shim/backbone.marionette.js
@@ -10,4 +10,9 @@ Backbone.Marionette.Renderer.render = function(template, data) {
     return template(data);
 };
 
+// Expose Backbone and Marionette for the Marionette Inspector
+if (window.__agent) {
+  window.__agent.start(Backbone, Marionette);
+}
+
 module.exports = Marionette;
