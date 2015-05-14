4c49fa567f150faaaf27fa1ae15eb059637fb8a2
Terence Tuhinanshu <ttuhinanshu@azavea.com>
2015-05-14 15:02:04 -0400

    Add ITSI login views
    There are two views:
     * itsi_login triggers the OAuth process. It constucts an auth_url
       using ItsiService and redirects the user to that authorization URL.
       Once there, the user will have to log in to the ITSI Portal, upon
       which they will be redirected to itsi_auth with a `code` query
       string paramter
     * itsi_auth receives the `code` from the query string, and feeds it
       to ItsiService to create an authenticated session with ITSI Portal.
       This session is then used to pull in user details. Once it has
       them, it checks to see if a user with the same ID already exists in
       the system. If not, a new user with that ID is created.
    
    These views do not actually log the user in, which is an extension
    that will be done for #128. Currently they simply create a new account
    if one doesn't exist already, otherwise they do nothing interesting.

--- src/mmw/apps/user/urls.py
+++ src/mmw/apps/user/urls.py
@@ -4,10 +4,12 @@ from __future__ import unicode_literals
 from __future__ import division
 
 from django.conf.urls import patterns, url
-from apps.user.views import ajax_login
+from apps.user.views import ajax_login, itsi_login, itsi_auth
 
 
 urlpatterns = patterns(
     '',
-    url(r'^ajaxlogin$', ajax_login, name='ajax_login')
+    url(r'^ajaxlogin$', ajax_login, name='ajax_login'),
+    url(r'^itsi/login$', itsi_login, name='itsi_login'),
+    url(r'^itsi/authenticate$', itsi_auth, name='itsi_auth'),
 )
--- src/mmw/apps/user/views.py
+++ src/mmw/apps/user/views.py
@@ -1,8 +1,16 @@
 from rest_framework.response import Response
 from django.contrib.auth import authenticate, login
+from django.core.urlresolvers import reverse
+from django.core.exceptions import ObjectDoesNotExist
+from django.shortcuts import redirect
+
+from django.contrib.auth.models import User
 
 from rest_framework import decorators
 from rest_framework.permissions import AllowAny
+
+from apps.user.models import ItsiUser
+from apps.user.itsi import ItsiService
 
 
 @decorators.api_view(['POST'])
@@ -25,5 +33,62 @@ def ajax_login(request):
     else:
         response_data['result'] = 'invalid'
         status = 400
+
+    return Response(data=response_data, status=status)
+
+
+@decorators.api_view(['GET'])
+@decorators.permission_classes((AllowAny, ))
+def itsi_login(request):
+    itsi = ItsiService()
+    redirect_uri = request.build_absolute_uri(reverse('itsi_auth'))
+    params = {'redirect_uri': redirect_uri}
+    auth_url = itsi.get_authorize_url(**params)
+
+    return redirect(auth_url)
+
+
+@decorators.api_view(['GET'])
+@decorators.permission_classes((AllowAny, ))
+def itsi_auth(request):
+    response_data = {}
+    status = 200
+
+    code = request.GET.get('code', None)
+
+    # Basic validation
+    if code is None:
+        response_data['result'] = 'invalid'
+        status = 400
+        return Response(data=response_data, status=status)
+
+    itsi = ItsiService()
+    session = itsi.get_session_from_code(code)
+    itsi_user = session.get_user()
+
+    # Check if user already exists
+    try:
+        user = ItsiUser.objects.get(itsi_id=itsi_user['id']).user
+
+    except ObjectDoesNotExist:
+        # User does not exist, create a new one
+        user = User.objects.create_user(
+            'itsi_' + itsi_user['extra']['username'],
+            itsi_user['info']['email'],
+            ''
+        )
+
+        user.first_name = itsi_user['extra']['first_name']
+        user.last_name = itsi_user['extra']['last_name']
+
+        new_itsi_user = ItsiUser.objects.create_itsi_user(user,
+                                                          itsi_user['id'])
+
+        user.save()
+        new_itsi_user.save()
+
+    response_data['result'] = 'success'
+    response_data['username'] = user.username
+    response_data['itsi'] = itsi_user
 
     return Response(data=response_data, status=status)
