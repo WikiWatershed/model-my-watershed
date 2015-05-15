60b65b730ce9d8154d49449002a8f934ec83996e
Matthew McFarland <mmcfarland@azavea.com>
2015-05-15 16:57:39 -0400

    Add Project and Scenario Models
    Projects are modeling containers for modified and unmodified scenarios
    of the same area of interest.

--- src/mmw/apps/modeling/models.py
+++ src/mmw/apps/modeling/models.py
@@ -6,6 +6,8 @@ from __future__ import division
 from django.contrib.gis.db import models
 from django.forms.models import model_to_dict
 
+from django.contrib.auth.models import User
+
 
 class District(models.Model):
     state_fips = models.CharField(
@@ -40,3 +42,55 @@ class District(models.Model):
         dictionary = model_to_dict(self)
         dictionary.pop('polygon', None)
         return str(dictionary)
+
+
+class Project(models.Model):
+    class Meta:
+        unique_together = ('name', 'user')
+
+    user = models.ForeignKey(User)
+    name = models.CharField(
+        max_length=255)
+    area_of_interest = models.MultiPolygonField(
+        help_text='Base geometry for all scenarios of project')
+    private = models.BooleanField(
+        default=True)
+    model_package = models.CharField(
+        max_length=255,
+        help_text='Which model pack was chosen for this project')
+    created = models.DateField(
+        auto_now=False,
+        auto_now_add=True)
+    modified = models.DateField(
+        auto_now=True)
+
+    def __unicode__(self):
+        return self.name
+
+
+class Scenario(models.Model):
+    class Meta:
+        unique_together = ('name', 'project')
+
+    name = models.CharField(
+        max_length=255)
+    project = models.ForeignKey(Project, related_name='scenarios')
+    current_condition = models.BooleanField(
+        default=False,
+        help_text='A special type of scenario without modification abilities')
+    modifications = models.TextField(
+        null=True,
+        help_text='Serialized JSON representation of this scenarios ' +
+                  'applied modification, with respect to the model package')
+    modification_hash = models.CharField(
+        max_length=255,
+        null=True,
+        help_text='A hash of the values for modifications & inputs to ' +
+                  'compare to the existing model results, to determine if ' +
+                  'the persisted result apply to the current values')
+    results = models.TextField(
+        null=True,
+        help_text='Serialized JSON representation of the model results')
+
+    def __unicode__(self):
+        return self.name
