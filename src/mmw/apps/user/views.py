81d920d346c5f9174a85492f87a16e45af12aa7a
Terence Tuhinanshu <ttuhinanshu@azavea.com>
2015-05-20 11:35:50 -0400

    Overload `/user/ajaxlogin` to get current user
    Add additional behavior to `/user/ajaxlogin` endpoint that will supply
    current user's credentials on GET if user is already logged in. If the
    user is not logged in, no credentials are sent, but the server sends a
    `guest` variable set to `true`.
    
    Also hook HeaderView's `render` action to the `sync` event, so that
    whenever we call `.fetch()` on the UserModel, the view is updated.

--- src/mmw/apps/user/views.py
+++ src/mmw/apps/user/views.py
@@ -5,7 +5,7 @@ from rest_framework import decorators
 from rest_framework.permissions import AllowAny
 
 
-@decorators.api_view(['POST'])
+@decorators.api_view(['POST', 'GET'])
 @decorators.permission_classes((AllowAny, ))
 def ajax_login(request):
     response_data = {}
@@ -19,9 +19,21 @@ def ajax_login(request):
                 login(request, user)
                 response_data['result'] = 'success'
                 response_data['username'] = user.username
+                response_data['guest'] = False
             else:
                 response_data['result'] = 'error'
                 status = 400
+    elif request.method == 'GET':
+        user = request.user
+
+        if user.is_authenticated() and user.is_active:
+            response_data['username'] = user.username
+            response_data['guest'] = False
+        else:
+            response_data['guest'] = True
+
+        response_data['result'] = 'success'
+        status = 200
     else:
         response_data['result'] = 'invalid'
         status = 400
