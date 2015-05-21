4490399b6f476330f7757e652c6101bc4b4ae15a
Laurence Liss <lliss@azavea.com>
2015-05-21 12:10:32 -0400

    Test enter key and header text on login.

--- src/mmw/js/src/user/tests.js
+++ src/mmw/js/src/user/tests.js
@@ -8,7 +8,10 @@ var _ = require('lodash'),
     sinon = require('sinon'),
     App = require('../app'),
     views = require('./views'),
-    models = require('./models');
+    models = require('./models'),
+    coreViews = require('../core/views');
+
+var ENTER_KEYCODE = 13;
 
 describe('UserModalLogin', function() {
     before(function() {
@@ -70,16 +73,70 @@ describe('UserModalLogin', function() {
             assert.equal(validationError, 'Incorrect username or password', 'Could not find failed login message.');
         });
         it('logs the user in when there is a valid response', function() {
+            var username = 'bob';
             this.server.respondWith([200, { 'Content-Type': 'application/json' }, '{"result": "success", "username": "bob" }']);
             var loginView = new views.LoginModalView({
                 el: '#sandbox',
                 model: new models.LoginFormModel({})
             }).render();
-            loginView.$el.find('#id_username').val('bob');
-            loginView.$el.find('#id_password').val('bob');
+            // Activate modal and make sure it is visible.
+            loginView.$el.modal('show');
+            assert.ok(loginView.$el.is(':visible'), 'Modal is not visible');
+
+            loginView.$el.find('#id_username').val(username);
+            loginView.$el.find('#id_password').val(username);
             loginView.$el.find('#sign-in').click();
+
             assert.notOk(loginView.$el.is(':visible'), 'Modal should no longer be visible.');
-            assert.equal(App.user.get('username'), 'bob', 'Could not get username.');
+            assert.equal(App.user.get('username'), username, 'Could not get username.');
+        });
+        it('activates the form submission when enter is pressed', function() {
+            var username = 'john';
+            this.server.respondWith([200, { 'Content-Type': 'application/json' }, '{"result": "success", "username": "' + username + '" }']);
+            var loginView = new views.LoginModalView({
+                el: '#sandbox',
+                model: new models.LoginFormModel({})
+            }).render();
+            var $headerContainer = $('<div>', {id: 'header-container'});
+            $('#sandbox').append($headerContainer);
+            var header = new coreViews.HeaderView({
+                el: '#header-container',
+                model: App.user
+            }).render();
+
+            // Activate modal and make sure it is visible.
+            loginView.$el.modal('show');
+            assert.ok(loginView.$el.is(':visible'), 'Modal is not visible');
+
+            loginView.$el.find('#id_username').val(username);
+            loginView.$el.find('#id_password').val(username);
+            var e = $.Event('keyup');
+            e.keyCode = ENTER_KEYCODE;
+            loginView.$el.find('#id_password').trigger(e);
+
+            assert.notOk(loginView.$el.is(':visible'), 'Modal should no longer be visible.');
+
+            // Make sure the header changes to show the username.
+            var match = _.find($('#sandbox .navigation ul.main li a.dropdown-toggle'), function(item) {
+               return $(item).text().trim() === username;
+            });
+            assert.ok(match, 'Username did not show up in the header');
+        });
+        it('should not dismiss the modal form if a non-enter key is pressed', function() {
+            var loginView = new views.LoginModalView({
+                el: '#sandbox',
+                model: new models.LoginFormModel({})
+            }).render();
+
+            // Activate modal and make sure it is visible.
+            loginView.$el.modal('show');
+            assert.ok(loginView.$el.is(':visible'), 'Modal is not visible');
+
+            var e = $.Event('keyup');
+            e.keyCode = 22;
+            loginView.$el.find('#id_password').trigger(e);
+
+            assert.ok(loginView.$el.is(':visible'), 'Modal still be visible.');
         });
     });
 });
