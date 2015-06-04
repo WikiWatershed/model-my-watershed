40d147ccb54c1d8477b7c6ba0a21b09e298606d3
Hector Castro <hcastro@azavea.com>
2015-06-04 11:51:02 -0400

    Add support for creating a VPC and Route 53 private hosted zone
    Make use of `majorkirby` to thread together the launching of an Amazon
    VPC, along with an associated Route 53 private hosted zone, with Boto.
    
    For more details instructions, see the `README` at the root of the
    `deployment` subdirectory.
    
    Connects #26.

---
+++ deployment/cfn/utils/constants.py
@@ -0,0 +1,20 @@
+EC2_INSTANCE_TYPES = [
+    't2.micro',
+    't2.small',
+    't2.medium'
+]
+
+RDS_INSTANCE_TYPES = [
+    'db.t2.micro'
+]
+
+ELASTICACHE_INSTANCE_TYPES = [
+    'cache.t2.micro'
+]
+
+ALLOW_ALL_CIDR = '0.0.0.0/0'
+VPC_CIDR = '10.0.0.0/16'
+
+SSH = 22
+HTTP = 80
+HTTPS = 443
