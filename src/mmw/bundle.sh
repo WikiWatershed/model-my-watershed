0e7aba8789c6e82cbc068048fa07ea9d2d1ef9f9
Kevin DeLoach <kdeloach@azavea.com>
2015-06-09 11:03:20 -0400

    Add turf-buffer and turf-random to vendor bundle
    Should reduce the primary bundle size from 1.9MB to 600KB.

--- src/mmw/bundle.sh
+++ src/mmw/bundle.sh
@@ -105,7 +105,7 @@ CONCAT_VENDOR_CSS_COMMAND="cat \
 JS_DEPS=(jquery backbone backbone.marionette \
          bootstrap bootstrap-select \
          leaflet leaflet-draw lodash underscore \
-         d3 nunjucks turf-area \
+         d3 nunjucks turf-area turf-buffer turf-random \
          zeroclipboard)
 BROWSERIFY_EXT=""
 BROWSERIFY_REQ=""
