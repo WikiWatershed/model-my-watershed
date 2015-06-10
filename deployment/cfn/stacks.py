3e789698a6aca29220bbb8e51335d3dbba60fe2c
Hector Castro <hcastro@azavea.com>
2015-06-10 15:56:52 -0400

    Add CustomActionNode to create private DNS entry for cache
    CloudFormation can create ElastiCache cache clusters, but it cannot
    retrieve a specific cache cluster node's endpoint. To bridge the gap,
    a `CustomActionNode` comes after the data plane stack to scoop up the
    cache cluster ID, use it to determine the cache cluster node's endpoint,
    and set it as a private `CNAME` record.
    
    Connects #339

--- deployment/cfn/stacks.py
+++ deployment/cfn/stacks.py
@@ -4,6 +4,7 @@ from vpc import VPC
 from s3_vpc_endpoint import S3VPCEndpoint
 from private_hosted_zone import PrivateHostedZone
 from data_plane import DataPlane
+from cache_private_dns_record import CachePrivateDNSRecord
 
 import ConfigParser
 
@@ -38,13 +39,20 @@ def build_graph(mmw_config, aws_profile, **kwargs):
     data_plane = DataPlane(globalconfig=global_config, VPC=vpc,
                            PrivateHostedZone=private_hosted_zone,
                            aws_profile=aws_profile)
+    cache_private_dns_record = CachePrivateDNSRecord(
+        globalconfig=global_config,
+        PrivateHostedZone=private_hosted_zone, DataPlane=data_plane,
+        aws_profile=aws_profile
+    )
 
-    return s3_vpc_endpoint, data_plane
+    return s3_vpc_endpoint, cache_private_dns_record
 
 
 def build_stacks(mmw_config, aws_profile, **kwargs):
     """Trigger actual building of graphs"""
-    s3_vpc_endpoint_graph, data_plane_graph = build_graph(mmw_config,
-                                                          aws_profile)
+    s3_vpc_endpoint_graph, cache_private_dns_record_graph = build_graph(
+        mmw_config,
+        aws_profile
+    )
     s3_vpc_endpoint_graph.go()
-    data_plane_graph.go()
+    cache_private_dns_record_graph.go()
