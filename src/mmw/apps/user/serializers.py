046c1b10faa1f2a4a8d9099f99bde5887195125b
James McClain <jmcclain@azavea.com>
2015-06-15 09:03:41 -0400

    Remove email from UserSerializer
    The `email` field has been removed from that serializer.
    
    Fixes #344

--- src/mmw/apps/user/serializers.py
+++ src/mmw/apps/user/serializers.py
@@ -9,4 +9,4 @@ from rest_framework import serializers
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('id', 'username', 'email', 'is_staff')
+        fields = ('id', 'username', 'is_staff')
