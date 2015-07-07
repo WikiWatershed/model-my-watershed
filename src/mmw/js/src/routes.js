ff92bb2602a0d59fb5887a8221791dae4c5174ba
Terence Tuhinanshu <ttuhinanshu@azavea.com>
2015-07-07 13:04:09 -0400

    Move CompareController to its own file
    And remove AppController, thus now we have no more generic controllers.

--- src/mmw/js/src/routes.js
+++ src/mmw/js/src/routes.js
@@ -1,12 +1,11 @@
 'use strict';
 
 var router = require('./router').router,
-    controllers = require('./controllers'),
-    AppController = controllers.AppController,
     ErrorController = require('./core/error/controllers').ErrorController,
     SignUpController = require('./user/controllers').SignUpController,
     DrawController = require('./draw/controllers').DrawController,
     ModelingController = require('./modeling/controllers').ModelingController,
+    CompareController = require('./compare/controllers').CompareController,
     AnalyzeController = require('./analyze/controllers').AnalyzeController;
 
 router.addRoute(/^/, DrawController, 'draw');
@@ -15,7 +14,7 @@ router.addRoute(/^project/, ModelingController, 'project');
 router.addRoute('project/:projectId', ModelingController, 'project');
 router.addRoute('project/:projectId/', ModelingController, 'project');
 router.addRoute('project/:projectId/scenario/:scenarioId/', ModelingController, 'project');
-router.addRoute(/^compare/, AppController, 'compare');
+router.addRoute(/^compare/, CompareController, 'compare');
 router.addRoute('error(/)(:type)', ErrorController, 'error');
 router.addRoute('sign-up(/)', SignUpController, 'signUp');
 router.addRoute('sign-up/itsi(/)(:username)(/:first_name)(/:last_name)', SignUpController, 'itsiSignUp');
