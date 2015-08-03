ff72ff02a53ad730c58e75ad17a6c464a00ce117
Terence Tuhinanshu <ttuhinanshu@azavea.com>
2015-08-03 14:01:51 -0400

    Load previously saved state
    If the `interactiveState` has been set at any point in the past, we may
    receive it via the `loadInteractive` postMessage. We check if this URL
    differs from the current one, and if so we check if a new project has
    been created for this user (since we are in the embed mode). If so, we
    delete the existing project. Finally, we redirect to the specified URL.

--- src/mmw/js/src/core/itsiEmbed.js
+++ src/mmw/js/src/core/itsiEmbed.js
@@ -2,6 +2,7 @@
 
 var _ = require('lodash'),
     iframePhone = require('iframe-phone'),
+    App = require('../app.js'),
     EMBED_FLAG = 'itsi_embed',
     QUERY_SUFFIX = EMBED_FLAG + '=true';
 
@@ -22,8 +23,31 @@ var ItsiEmbed = function() {
         this.phone.post('interactiveState', this.interactiveState);
     };
 
+    this.loadInteractive = function(interactiveState) {
+        if (interactiveState && interactiveState.url) {
+            // Only redirect if different URL
+            if (window.location.href !== interactiveState.url) {
+                if (App.currProject &&
+                    !App.currProject.isNew() &&
+                    !App.currProject.get('area_of_interest')) {
+                    // Delete automatically created new project
+                    App.currProject
+                        .destroy()
+                        .always(function() {
+                            // Redirect after deletion
+                            window.location.href = interactiveState.url;
+                        });
+                } else {
+                    // No current project, just redirect
+                    window.location.href = interactiveState.url;
+                }
+            }
+        }
+    };
+
     this.phone.addListener('getLearnerUrl', _.bind(this.setLearnerUrl, this));
     this.phone.addListener('getInteractiveState', _.bind(this.setLearnerUrl, this));
+    this.phone.addListener('loadInteractive', _.bind(this.loadInteractive, this));
     this.phone.initialize();
 };
 
