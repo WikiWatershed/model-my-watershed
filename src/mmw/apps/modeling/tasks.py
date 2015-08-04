16d987f2540a0a45329005152f6e5176985416c1
Laurence Liss <lliss@azavea.com>
2015-08-04 11:02:17 -0400

    Make modeling results return metric numbers.
    Modeling results have been returned in Imperial units and the coverage area
    was returned in the number of pixels in the analyzed raster. This is not a
    useful number for most users. We convert the results to metric units prior to
    returning the results. Specifically, the coverage requires information about
    the analyzed area so that it can roughly calculate the area of a pixel based
    on the latitude.

--- src/mmw/apps/modeling/tasks.py
+++ src/mmw/apps/modeling/tasks.py
@@ -4,7 +4,9 @@ from __future__ import unicode_literals
 from __future__ import absolute_import
 
 from celery import shared_task
+import json
 import logging
+import math
 
 # TODO Remove this when stub task is deleted.
 import time
@@ -14,9 +16,23 @@ from tr55.tablelookup import lookup_ki
 
 logger = logging.getLogger(__name__)
 
+KG_PER_POUND = 0.453592
+CM_PER_INCH = 2.54
+
 
 @shared_task
 def run_analyze(area_of_interest):
+
+    # Find the width in meters of a pixel at the average lat of the shape.
+    pairs = json.loads(area_of_interest)['coordinates'][0][0]
+    average_lat = reduce(lambda total, p: total+p[1], pairs, 0) / len(pairs)
+
+    # Zoom level is a hard coded value in the geoprocessing library.
+    zoom = 13
+    # See https://msdn.microsoft.com/en-us/library/bb259689.aspx
+    pixel_width = (math.cos(average_lat * math.pi / 180) * 2 * math.pi *
+                   6378137) / (256 * math.pow(2, zoom))
+
     time.sleep(3)
 
     results = [
@@ -114,6 +130,8 @@ def run_analyze(area_of_interest):
         }
     ]
 
+    convert_result_areas(pixel_width, results)
+
     return results
 
 
@@ -163,10 +181,28 @@ def format_quality(model_output):
         measure, code = input
         return {
             'measure': measure,
-            'load': model_output['modified'][code]
+            'load': model_output['modified'][code] * KG_PER_POUND
         }
 
     return map(fn, zip(measures, codes))
+
+
+def format_runoff(model_output):
+    """
+    Convert model output values that are in inches into centimeters.
+    """
+    inch_keys = ['runoff', 'inf', 'et']
+    for key in model_output:
+        for item in inch_keys:
+            model_output[key][item] = model_output[key][item] * CM_PER_INCH
+
+        distribution = model_output[key]['distribution']
+        for k in distribution:
+            for item in inch_keys:
+                model_output[key]['distribution'][k][item] = \
+                    model_output[key]['distribution'][k][item] * CM_PER_INCH
+
+    return model_output
 
 
 @shared_task
@@ -196,7 +232,7 @@ def run_tr55(census, model_input):
     return {
         'inputmod_hash': model_input['inputmod_hash'],
         'census': census,
-        'runoff': model_output,
+        'runoff': format_runoff(model_output),
         'quality': format_quality(model_output)
     }
 
@@ -208,6 +244,18 @@ def _get_precip_value(model_input):
         return precips[0]['value']
     except Exception:
         return None
+
+
+def convert_result_areas(pixel_width, results):
+    """
+    Updates area values on the survey results. The survey gives area as a
+    number of pixels in a geotiff. This converts the area to square meters.
+    """
+    for i in range(len(results)):
+        categories = results[i]['categories']
+        for j in range(len(categories)):
+            categories[j]['area'] = (categories[j]['area'] * pixel_width *
+                                     pixel_width)
 
 
 # TODO: For demonstration purposes.
