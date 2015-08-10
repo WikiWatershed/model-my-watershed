e7cefa4ff16967d0a2f229313f7c21cc4c516369
James McClain <jmcclain@azavea.com>
2015-08-10 11:40:03 -0400

    Remove landMap from mini-app.
    Them mini-app no longer needs this translation table because it now uses
    the same internal names for land covers as are found in the model.csv
    tables (which are also the same names as used in the TR-55 module).

--- src/mmw/js/src/water_balance/models.js
+++ src/mmw/js/src/water_balance/models.js
@@ -1,21 +1,6 @@
 "use strict";
 
 var WaterBalanceModel = {
-    landMap: {
-        'urban_grass': ['urban_grass'],
-        'tall_grass_prairie': ['tall_grass_prairie'],
-        'li_residential': ['li_residential'],
-        'hi_residential': ['hi_residential'],
-        'commercial': ['commercial'],
-        'desert': ['desert'],
-        'deciduous_forest': ['deciduous_forest'],
-        'chaparral': ['chaparral'],
-        'grassland': ['grassland'],
-        'pasture': ['pasture'],
-        'short_grass_prairie': ['short_grass_prairie'],
-        'row_crop': ['row_crop']
-    },
-
     soilMap: [
         'sand',
         'loam',
@@ -31,30 +16,28 @@ var WaterBalanceModel = {
             return Math.round(parseFloat(x) * 10) / 10;
         }
 
-        function processLine(soil, lands, precip) {
+        function processLine(soil, land, precip) {
             if (!model[soil]) {
                 model[soil] = {};
             }
-            lands.forEach(function(land) {
-                if (!model[soil][land]) {
-                    model[soil][land] = {};
-                }
-                model[soil][land][precip] = {
-                    'et': round(line[3]),
-                    'i' : round(line[4]),
-                    'r' : round(line[5])
-                };
-            });
+            if (!model[soil][land]) {
+                model[soil][land] = {};
+            }
+            model[soil][land][precip] = {
+                'et': round(line[3]),
+                'i' : round(line[4]),
+                'r' : round(line[5])
+            };
         }
 
         for (var j = 1; j < lines.length; j++) {
             var line = lines[j].split(',');
             var soil = this.soilMap[line[2]];
-            var lands = this.landMap[line[1]];
+            var land = line[1];
             var precip = line[0];
 
-            if (soil && lands && precip) {
-                processLine(soil, lands, precip);
+            if (soil && land && precip) {
+                processLine(soil, land, precip);
             }
         }
 
