c1fd1fc61ec9e2c08c526f6356130b1d0800ec82
Terence Tuhinanshu <ttuhinanshu@azavea.com>
2015-10-05 18:07:03 -0400

    Translate mockup into views
     * Add ProjectCollection which pulls from the new endpoint
     * Extract a project row into its own template and use it to populate a
       list of projects available to the user.
     * Add a `toFriendlyDate` filter which formats dates nicely
     * Make project list scrollable

--- src/mmw/js/src/projects/views.js
+++ src/mmw/js/src/projects/views.js
@@ -1,10 +1,37 @@
 "use strict";
 
 var Marionette = require('../../shim/backbone.marionette'),
-    containerTmpl = require('./templates/container.html');
+    models = require('../modeling/models'),
+    containerTmpl = require('./templates/container.html'),
+    rowTmpl = require('./templates/projectRow.html');
 
 var ProjectsView = Marionette.LayoutView.extend({
     template: containerTmpl,
+
+    regions: {
+        rowContainer: '.project-rows-container'
+    },
+
+    onShow: function() {
+        var self = this,
+            projects = new models.ProjectCollection();
+        projects
+            .fetch()
+            .done(function() {
+               self.rowContainer.show(new ProjectRowsView({
+                    collection: projects
+               }));
+            });
+    }
+});
+
+var ProjectRowView = Marionette.ItemView.extend({
+    className: 'project row',
+    template: rowTmpl
+});
+
+var ProjectRowsView = Marionette.CollectionView.extend({
+    childView: ProjectRowView
 });
 
 module.exports = {
