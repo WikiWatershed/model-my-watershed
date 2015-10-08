e102055d8fc89e4046aac7c2cc9011d8564936ee
James McClain <jmcclain@azavea.com>
2015-10-08 13:49:55 -0400

    Allow Tests to Pass
    The specific queuing has been removed from the tasks that do not
    interact directly with spark-jobserver.  This allows tests to continue
    to pass, and gives Celery more flexibility to schedule jobs.

--- src/mmw/apps/modeling/views.py
+++ src/mmw/apps/modeling/views.py
@@ -29,6 +29,10 @@ from apps.modeling.serializers import (ProjectSerializer,
                                        ProjectUpdateSerializer,
                                        ScenarioSerializer)
 
+# When CELERY_WORKER_DIRECT = True, this exchange is automatically
+# created to allow direct communication with workers.
+MAGIC_EXCHANGE = 'C.dq'
+
 
 @decorators.api_view(['GET', 'POST'])
 @decorators.permission_classes((IsAuthenticated, ))
@@ -204,17 +208,15 @@ def choose_worker():
 
 
 def _initiate_analyze_job_chain(area_of_interest, job_id, testing=False):
-    exchange = 'C.dq'
+    exchange = MAGIC_EXCHANGE
     routing_key = choose_worker()
 
-    return chain(tasks.start_histogram_job.s(area_of_interest).set(
-                    exchange=exchange, routing_key=routing_key),
-                 tasks.get_histogram_job_results.s().set(
-                     exchange=exchange, routing_key=routing_key),
-                 tasks.histogram_to_survey.s().set(
-                     exchange=exchange, routing_key=routing_key),
-                 save_job_result.s(job_id, area_of_interest).set(
-                     exchange=exchange, routing_key=routing_key)) \
+    return chain(tasks.start_histogram_job.s(area_of_interest)
+                 .set(exchange=exchange, routing_key=routing_key),
+                 tasks.get_histogram_job_results.s()
+                 .set(exchange=exchange, routing_key=routing_key),
+                 tasks.histogram_to_survey.s(),
+                 save_job_result.s(job_id, area_of_interest)) \
         .apply_async(link_error=save_job_error.s(job_id))
 
 
@@ -244,7 +246,7 @@ def _initiate_tr55_job_chain(model_input, job_id):
 
 
 def _construct_tr55_job_chain(model_input, job_id):
-    exchange = 'C.dq'
+    exchange = MAGIC_EXCHANGE
     routing_key = choose_worker()
 
     job_chain = []
@@ -269,32 +271,28 @@ def _construct_tr55_job_chain(model_input, job_id):
        census_hash == current_hash) or not pieces)):
         censuses = [aoi_census] + modification_census_items
 
-        job_chain.append(tasks.run_tr55.s(censuses, model_input).set(
-            exchange=exchange, routing_key=routing_key))
+        job_chain.append(tasks.run_tr55.s(censuses, model_input))
     else:
-        job_chain.append(tasks.get_histogram_job_results.s().set(
-            exchange=exchange, routing_key=routing_key))
-        job_chain.append(tasks.histograms_to_censuses.s().set(
-            exchange=exchange, routing_key=routing_key))
+        job_chain.append(tasks.get_histogram_job_results.s()
+                         .set(exchange=exchange, routing_key=routing_key))
+        job_chain.append(tasks.histograms_to_censuses.s()
+                         .set(exchange=exchange, routing_key=routing_key))
 
         if aoi_census and pieces:
             polygons = [m['shape']['geometry'] for m in pieces]
 
-            job_chain.insert(0, tasks.start_histograms_job.s(polygons).set(
-                    exchange=exchange, routing_key=routing_key))
+            job_chain.insert(0, tasks.start_histograms_job.s(polygons)
+                             .set(exchange=exchange, routing_key=routing_key))
             job_chain.insert(len(job_chain), tasks.run_tr55.s(model_input,
-                             cached_aoi_census=aoi_census).set(
-                                 exchange=exchange, routing_key=routing_key))
+                             cached_aoi_census=aoi_census))
         else:
             polygons = [aoi] + [m['shape']['geometry'] for m in pieces]
 
-            job_chain.insert(0, tasks.start_histograms_job.s(polygons).set(
-                exchange=exchange, routing_key=routing_key))
-            job_chain.insert(len(job_chain), tasks.run_tr55.s(model_input).set(
-                exchange=exchange, routing_key=routing_key))
+            job_chain.insert(0, tasks.start_histograms_job.s(polygons)
+                             .set(exchange=exchange, routing_key=routing_key))
+            job_chain.insert(len(job_chain), tasks.run_tr55.s(model_input))
 
-    job_chain.append(save_job_result.s(job_id, model_input).set(
-        exchange=exchange, routing_key=routing_key))
+    job_chain.append(save_job_result.s(job_id, model_input))
 
     return job_chain
 
