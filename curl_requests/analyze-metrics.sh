ab0b9190415d2b81f8c4859ee68344ae6ffaca6c
Laurence Liss <lliss@azavea.com>
2015-12-01 17:28:45 -0500

    Add shell scripts.
    These shell scripts allow for easy testing.

---
+++ curl_requests/analyze-metrics.sh
@@ -0,0 +1,44 @@
+!#/bin/bash
+
+printf "\nBegin Analyze 1 Km\n";
+curl 'http://localhost:8000/api/modeling/start/analyze/' --data @analyze-1.txt --compressed;
+sleep 30;
+
+printf "\nBegin Analyze 16 Km\n";
+curl 'http://localhost:8000/api/modeling/start/analyze/' --data @analyze-2.txt --compressed;
+sleep 30;
+
+printf "\nBegin Analyze 32 Km\n";
+curl 'http://localhost:8000/api/modeling/start/analyze/' --data @analyze-3.txt --compressed;
+sleep 30;
+
+printf "\nBegin Analyze 60 Km\n";
+curl 'http://localhost:8000/api/modeling/start/analyze/' --data @analyze-4.txt --compressed;
+sleep 30;
+
+printf "\nBegin Analyze 121 Km\n";
+curl 'http://localhost:8000/api/modeling/start/analyze/' --data @analyze-5.txt --compressed;
+sleep 30;
+
+printf "\nBegin Analyze 274 Km\n";
+curl 'http://localhost:8000/api/modeling/start/analyze/' --data @analyze-6.txt --compressed;
+sleep 30;
+
+printf "\nBegin Analyze 512 Km\n";
+curl 'http://localhost:8000/api/modeling/start/analyze/' --data @analyze-7.txt --compressed;
+sleep 30;
+
+printf "\nBegin Analyze 1091 Km\n";
+curl 'http://localhost:8000/api/modeling/start/analyze/' --data @analyze-8.txt --compressed;
+sleep 1m;
+
+printf "\nBegin Analyze 2019 Km\n";
+curl 'http://localhost:8000/api/modeling/start/analyze/' --data @analyze-9.txt --compressed;
+sleep 1m;
+
+printf "\nBegin Analyze 4130 Km\n";
+curl 'http://localhost:8000/api/modeling/start/analyze/' --data @analyze-10.txt --compressed;
+#sleep 2m;
+
+#printf "\nBegin Analyze 8018 Km\n";
+#curl 'http://localhost:8000/api/modeling/start/analyze/' --data @analyze-11.txt --compressed;
