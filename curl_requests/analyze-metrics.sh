32387af9e29603a9a68d579babca35d5784193b0
Hector Castro <hcastro@azavea.com>
2015-12-02 11:26:01 -0500

    Enable debug output; make API endpoint configurable

--- curl_requests/analyze-metrics.sh
+++ curl_requests/analyze-metrics.sh
@@ -1,44 +1,49 @@
-!#/bin/bash
+#!/bin/bash
+
+set -e
+set -x
+
+API_ENDPOINT="mmw-dev.azavea.com"
 
 printf "\nBegin Analyze 1 Km\n";
-curl 'http://localhost:8000/api/modeling/start/analyze/' --data @analyze-1.txt --compressed;
+curl "https://${API_ENDPOINT}/api/modeling/start/analyze/" --data @analyze-1.txt --compressed;
 sleep 30;
 
 printf "\nBegin Analyze 16 Km\n";
-curl 'http://localhost:8000/api/modeling/start/analyze/' --data @analyze-2.txt --compressed;
+curl "https://${API_ENDPOINT}/api/modeling/start/analyze/" --data @analyze-2.txt --compressed;
 sleep 30;
 
 printf "\nBegin Analyze 32 Km\n";
-curl 'http://localhost:8000/api/modeling/start/analyze/' --data @analyze-3.txt --compressed;
+curl "https://${API_ENDPOINT}/api/modeling/start/analyze/" --data @analyze-3.txt --compressed;
 sleep 30;
 
 printf "\nBegin Analyze 60 Km\n";
-curl 'http://localhost:8000/api/modeling/start/analyze/' --data @analyze-4.txt --compressed;
+curl "https://${API_ENDPOINT}/api/modeling/start/analyze/" --data @analyze-4.txt --compressed;
 sleep 30;
 
 printf "\nBegin Analyze 121 Km\n";
-curl 'http://localhost:8000/api/modeling/start/analyze/' --data @analyze-5.txt --compressed;
+curl "https://${API_ENDPOINT}/api/modeling/start/analyze/" --data @analyze-5.txt --compressed;
 sleep 30;
 
 printf "\nBegin Analyze 274 Km\n";
-curl 'http://localhost:8000/api/modeling/start/analyze/' --data @analyze-6.txt --compressed;
+curl "https://${API_ENDPOINT}/api/modeling/start/analyze/" --data @analyze-6.txt --compressed;
 sleep 30;
 
 printf "\nBegin Analyze 512 Km\n";
-curl 'http://localhost:8000/api/modeling/start/analyze/' --data @analyze-7.txt --compressed;
+curl "https://${API_ENDPOINT}/api/modeling/start/analyze/" --data @analyze-7.txt --compressed;
 sleep 30;
 
 printf "\nBegin Analyze 1091 Km\n";
-curl 'http://localhost:8000/api/modeling/start/analyze/' --data @analyze-8.txt --compressed;
+curl "https://${API_ENDPOINT}/api/modeling/start/analyze/" --data @analyze-8.txt --compressed;
 sleep 1m;
 
 printf "\nBegin Analyze 2019 Km\n";
-curl 'http://localhost:8000/api/modeling/start/analyze/' --data @analyze-9.txt --compressed;
+curl "https://${API_ENDPOINT}/api/modeling/start/analyze/" --data @analyze-9.txt --compressed;
 sleep 1m;
 
 printf "\nBegin Analyze 4130 Km\n";
-curl 'http://localhost:8000/api/modeling/start/analyze/' --data @analyze-10.txt --compressed;
+curl "https://${API_ENDPOINT}/api/modeling/start/analyze/" --data @analyze-10.txt --compressed;
 #sleep 2m;
 
 #printf "\nBegin Analyze 8018 Km\n";
-#curl 'http://localhost:8000/api/modeling/start/analyze/' --data @analyze-11.txt --compressed;
+#curl "https://${API_ENDPOINT}/api/modeling/start/analyze/" --data @analyze-11.txt --compressed;
