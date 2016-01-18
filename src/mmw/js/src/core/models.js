87d3eddac9e63fe159dcf999493bc109fe7f2587
jfrankl <jfrankl@azavea.com>
2016-01-18 15:26:27 -0500

    Add map sizing/positioning for sidebar

--- src/mmw/js/src/core/models.js
+++ src/mmw/js/src/core/models.js
@@ -65,7 +65,7 @@ var MapModel = Backbone.Model.extend({
     },
 
     setDoubleHeaderHalfFooterSize: function(fit) {
-        this._setSizeOptions({ double: true  }, { large: true }, fit);
+        this._setSizeOptions({ sidebar: true  }, { sidebar: true }, fit);
     },
 
     setDrawSize: function(fit) {
