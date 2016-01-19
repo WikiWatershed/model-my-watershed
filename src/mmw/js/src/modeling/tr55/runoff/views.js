a556c664b9fc26d831f9b8b811cbdedcfc5699e1
Matthew McFarland <mmcfarland@azavea.com>
2016-01-19 11:33:45 -0500

    Add table to runoff results
    Represent the data the water depth chart also in table forms, including
    the additional non-modifed column (either 100% Forest or Current
    conditions depending on the scenario).

--- src/mmw/js/src/modeling/tr55/runoff/views.js
+++ src/mmw/js/src/modeling/tr55/runoff/views.js
@@ -2,21 +2,58 @@
 
 var $ = require('jquery'),
     _ = require('underscore'),
+    Backbone = require('backbone'),
     Marionette = require('../../../../shim/backbone.marionette'),
     chart = require('../../../core/chart.js'),
-    barChartTmpl = require('../../../core/templates/barChart.html');
+    barChartTmpl = require('../../../core/templates/barChart.html'),
+    resultTmpl = require('./templates/result.html'),
+    tableRowTmpl = require('./templates/tableRow.html'),
+    tableTmpl = require('./templates/table.html');
 
-var ResultView = Marionette.ItemView.extend({
-    template: barChartTmpl,
+var ResultView = Marionette.LayoutView.extend({
+    template: resultTmpl,
 
-    className: 'chart-container runoff-chart-container',
+    regions: {
+        tableRegion: '.runoff-table-region',
+        chartRegion: '.runoff-chart-region'
+    },
 
     modelEvents: {
-        'change': 'addChart'
+        'change': 'onShow'
     },
 
     initialize: function(options) {
-        this.compareMode = options.compareMode;
+        this.scenario = options.scenario;
+    },
+
+    onShow: function() {
+        this.tableRegion.reset();
+        this.chartRegion.reset();
+
+        if (this.model.get('result')) {
+            var dataCollection = new Backbone.Collection(
+                this.model.get('result')
+            );
+
+            this.tableRegion.show(new TableView({
+                scenario: this.scenario,
+                data: dataCollection
+            }));
+
+            this.chartRegion.show(new ChartView({
+                model: this.model,
+                scenario: this.scenario,
+                collection: dataCollection
+            }));
+        }
+    }
+});
+
+var ChartView = Marionette.ItemView.extend({
+    template: barChartTmpl,
+    className: 'chart-container runoff-chart-container',
+
+    initialize: function(options) {
         this.scenario = options.scenario;
     },
 
@@ -86,6 +123,74 @@ var ResultView = Marionette.ItemView.extend({
 
             chart.renderVerticalBarChart(chartEl, data, chartOptions);
         }
+    }
+
+});
+
+var TableRowView = Marionette.ItemView.extend({
+    tagName: 'tr',
+    template: tableRowTmpl
+});
+
+var TableView = Marionette.CompositeView.extend({
+    childView: TableRowView,
+    childViewContainer: 'tbody',
+    template: tableTmpl,
+
+    runoffTypes: [
+        { name: 'runoff', display: 'Runoff' },
+        { name: 'et', display: 'Evapotranspiration' },
+        { name: 'inf', display: 'Infiltration' }
+    ],
+
+    initialize: function(options) {
+        this.collection = this.formatData(options.data, options.scenario);
+    },
+
+    onAttach: function() {
+        $('[data-toggle="table"]').bootstrapTable();
+    },
+
+    formatData: function(data, scenario) {
+        // The TR55 results should be broken down into:
+        // Scenario | Runoff Type | Depth
+        var tr55Results = data.first().attributes,
+            collection = new Backbone.Collection(),
+            // If not Current Conditions, match the graph by calling
+            // the scenario "modified"
+            label = 'Modified';
+
+        // Special cases:  100% Forest may exist as pc_unmodified if scenario
+        // is_current_conditions, otherwise we also want to include unmodified
+        // which is the value of `Current Conditions`.
+        if (scenario.get('is_current_conditions')) {
+            collection.add(this.makeRowsForScenario(tr55Results.pc_unmodified,
+                '100% Forest'));
+            label = scenario.get('name');
+        } else {
+            collection.add(this.makeRowsForScenario(tr55Results.unmodified,
+                'Current Conditions'));
+        }
+
+        collection.add(this.makeRowsForScenario(tr55Results.modified, label));
+
+        return collection;
+    },
+
+    makeRowsForScenario: function(tr55Results, scenarioName) {
+        var self = this;
+        return _.map(this.runoffTypes, function(runoffType) {
+            return _.extend(self.getRunoffTypeValue(tr55Results, runoffType), {
+                scenario: scenarioName
+            });
+        });
+    },
+
+    getRunoffTypeValue: function(tr55ResultRow, runoffType) {
+        return {
+            runoffType: runoffType.display,
+            depth: tr55ResultRow[runoffType.name]
+        };
     }
 });
 
