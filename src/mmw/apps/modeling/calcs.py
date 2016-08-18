4dc6874752b8ca8858de176fe868134ac13226bc
Kelly Innes <kinnes@azavea.com>
2016-08-18 15:49:46 -0400

    Return AEU with Analyze results
    - add `calcs.py` file in main modeling dir
    - add function to wrap a call to MapShed's `animal_energy_units` method and to structure the animal population results for the outgoing json file
    - use input polygon in `tasks.py` file to retrieve animal population results

---
+++ src/mmw/apps/modeling/calcs.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+from __future__ import print_function
+from __future__ import unicode_literals
+from __future__ import absolute_import
+
+from django.contrib.gis.geos import GEOSGeometry
+
+from django.conf import settings
+
+from apps.modeling.mapshed.calcs import animal_energy_units
+
+ANIMAL_KEYS = settings.GWLFE_CONFIG['AnimalKeys']
+ANIMAL_NAMES = settings.GWLFE_DEFAULTS['AnimalName']
+
+ANIMAL_DISPLAY_NAMES = dict(zip(ANIMAL_KEYS, ANIMAL_NAMES))
+
+
+def animal_population(geojson):
+    """
+    Given a GeoJSON shape, call MapShed's `animal_energy_units` method
+    to calculate the area-weighted county animal population. Returns a
+    dictionary to append to the outgoing JSON for analysis results.
+    """
+    geom = GEOSGeometry(geojson, srid=4326)
+    aeu_for_geom = animal_energy_units(geom)[2]
+    aeu_return_values = []
+
+    for animal, aeu_value in aeu_for_geom.iteritems():
+        aeu_return_values.append({
+            'type': ANIMAL_DISPLAY_NAMES[animal],
+            'aeu': int(round(aeu_value)),
+        })
+
+    return {
+        'displayName': 'Animals',
+        'name': 'animals',
+        'categories': aeu_return_values
+    }
