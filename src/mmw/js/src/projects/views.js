3425fff6de18b15034718747de5fcc6876cba0ba
Casey Cesari <ccesari@azavea.com>
2017-03-24 12:04:55 -0400

    Clear out analyze results when opening project
    Ensure that any previous analyze results are cleared out before a saved
    project is loaded. When the saved project is loaded, the app will now
    run analyze results for the project AoI.
    
    Refs #1768

--- src/mmw/js/src/projects/views.js
+++ src/mmw/js/src/projects/views.js
@@ -63,14 +63,16 @@ var ProjectRowView = Marionette.ItemView.extend({
         rename: '.btn-rename',
         share: '.btn-share',
         privacy: '.btn-privacy',
-        remove: '.btn-delete'
+        remove: '.btn-delete',
+        open: '.open-project'
     },
 
     events: {
         'click @ui.rename': 'renameProject',
         'click @ui.share': 'shareProject',
         'click @ui.privacy': 'setProjectPrivacy',
-        'click @ui.remove': 'deleteProject'
+        'click @ui.remove': 'deleteProject',
+        'click @ui.open': 'openProject'
     },
 
     modelEvents: {
@@ -162,6 +164,11 @@ var ProjectRowView = Marionette.ItemView.extend({
                     alertView.render();
                 });
         });
+    },
+
+    openProject: function() {
+        App.clearAnalyzeCollection();
+        router.navigate('/project/' + this.model.id, { trigger: true });
     }
 });
 
