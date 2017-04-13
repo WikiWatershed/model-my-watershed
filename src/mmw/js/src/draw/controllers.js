44858d6a8979352c1d632d0d88f045356a7d920f
Alice Rottersman <arottersman@azavea.com>
2017-04-13 09:29:55 -0400

    Move AOI Upload Into Sidebar
    - Remove draw toolbar
    - Move aoi upload drag 'n drop span into sidebar
    - Wire up "Select file" button

--- src/mmw/js/src/draw/controllers.js
+++ src/mmw/js/src/draw/controllers.js
@@ -15,16 +15,12 @@ var DrawController = {
 
     draw: function() {
         var geocodeSearch = new geocoder.GeocoderView(),
-            toolbarModel = new models.ToolbarModel(),
-            toolbarView = new views.ToolbarView({
-                model: toolbarModel
-            });
+            toolbarModel = new models.ToolbarModel();
 
         toolbarModel.set('predefinedShapeTypes',
             _.filter(settings.get('boundary_layers'), { selectable: true }));
 
         App.rootView.geocodeSearchRegion.show(geocodeSearch);
-        App.rootView.drawToolsRegion.show(toolbarView);
         App.rootView.sidebarRegion.show(new views.DrawWindow({
             model: toolbarModel
         }));
@@ -37,7 +33,6 @@ var DrawController = {
 
     drawCleanUp: function() {
         App.rootView.geocodeSearchRegion.empty();
-        App.rootView.drawToolsRegion.empty();
         App.rootView.footerRegion.empty();
     },
 
