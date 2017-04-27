7f0111fa2f024916067ee371a245478f67f9c058
Kevin DeLoach <kdeloach@azavea.com>
2017-04-27 16:18:02 -0400

    Implement CUAHSI search
    Execute SOAP request against CUAHSI hiscentral API for BiG CZ.

--- src/mmw/apps/bigcz/clients/__init__.py
+++ src/mmw/apps/bigcz/clients/__init__.py
@@ -3,9 +3,10 @@ from __future__ import print_function
 from __future__ import unicode_literals
 from __future__ import division
 
-from . import hydroshare
+from . import hydroshare, cuahsi
 
 
 SEARCH_FUNCTIONS = {
     hydroshare.CATALOG_NAME: hydroshare.search,
+    cuahsi.CATALOG_NAME: cuahsi.search,
 }
