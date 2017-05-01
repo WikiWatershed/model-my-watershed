d412375acde21da7f8bf01ff266383413abeade7
Kevin DeLoach <kdeloach@azavea.com>
2017-05-01 13:42:48 -0400

    Implement CINERGI search
    Execute CINERGI search against Geoportal REST API for BiG CZ.

--- src/mmw/apps/bigcz/clients/__init__.py
+++ src/mmw/apps/bigcz/clients/__init__.py
@@ -3,10 +3,11 @@ from __future__ import print_function
 from __future__ import unicode_literals
 from __future__ import division
 
-from . import hydroshare, cuahsi
+from . import hydroshare, cuahsi, cinergi
 
 
 SEARCH_FUNCTIONS = {
     hydroshare.CATALOG_NAME: hydroshare.search,
     cuahsi.CATALOG_NAME: cuahsi.search,
+    cinergi.CATALOG_NAME: cinergi.search,
 }
