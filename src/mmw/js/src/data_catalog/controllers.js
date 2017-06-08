4c5c9d77cf27fac3579b27b5110cc0011eef4c99
Kevin DeLoach <kdeloach@azavea.com>
2017-06-08 15:59:14 -0400

    Improve BiG CZ list view
    Revamp BiG CZ sidebar by replacing vanilla Backbone with Marionette
    views. I was able to reuse a lot of code from the Analyze view for this
    update. Some UI changes were also made to match the latest design
    mockups.
    
    Changes:
    
    * Replace vanilla Backbone with Marionette
    * Remove hard-coded catalog definitions from templates
    * Implement tabs and deferred search requests
    * Added optional catalog descriptions below each tab
    * Better request and error handling
    * Revert wide sidebar
    
    Connects #1935

--- src/mmw/js/src/data_catalog/controllers.js
+++ src/mmw/js/src/data_catalog/controllers.js
@@ -14,10 +14,32 @@ var DataCatalogController = {
             'was_data_catalog_visible': true,
         });
 
-        var searchModel = new models.DataCatalogModel();
-        var resources = new models.ResourceCollection();
+        var form = new models.SearchForm();
 
-        var view = new views.DataCatalogWindow(searchModel, resources);
+        var catalogs = new models.Catalogs([
+            new models.Catalog({
+                id: 'cinergi',
+                name: 'CINERGI',
+                active: true,
+                results: new models.Results()
+            }),
+            new models.Catalog({
+                id: 'hydroshare',
+                name: 'HydroShare',
+                results: new models.Results()
+            }),
+            new models.Catalog({
+                id: 'cuahsi',
+                name: 'WDC',
+                description: 'Optional catalog description here...',
+                results: new models.Results()
+            })
+        ]);
+
+        var view = new views.DataCatalogWindow({
+            model: form,
+            collection: catalogs
+        });
         App.rootView.sidebarRegion.show(view);
     }
 };
