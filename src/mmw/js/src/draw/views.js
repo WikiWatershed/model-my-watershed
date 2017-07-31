b0809400b17e0b164b723889d2b39588e89a1375
Terence Tuhinanshu <ttuhinanshu@azavea.com>
2017-07-31 14:01:42 -0400

    Minor fixes
     * Adjust error language to say "selected area" instead of "area
       you have delineated", since the error message may be displayed
       for boundary selection as well as watershed delineation.
     * Change " " → "nbsp;" between value and units
     * Change RWD error modal to Error from Warn to match all other
       draw tools.

--- src/mmw/js/src/draw/views.js
+++ src/mmw/js/src/draw/views.js
@@ -104,10 +104,10 @@ function validateShape(polygon) {
     } else if (!utils.isValidForAnalysis(polygon)) {
         var maxArea = utils.MAX_AREA.toLocaleString(),
             selectedBoundingBoxArea = Math.floor(utils.shapeBoundingBoxArea(polygon)).toLocaleString(),
-            message = 'Sorry, the bounding box of the area you have delineated is too large ' +
-                      'to analyze or model. ' + selectedBoundingBoxArea + ' km² were ' +
+            message = 'Sorry, the bounding box of the selected area is too large ' +
+                      'to analyze or model. ' + selectedBoundingBoxArea + '&nbsp;km² were ' +
                       'selected, but the maximum supported size is ' +
-                      'currently ' + maxArea + ' km².';
+                      'currently ' + maxArea + '&nbsp;km².';
         d.reject(message);
     } else {
         d.resolve(polygon);
@@ -874,7 +874,7 @@ var WatershedDelineationView = DrawToolBaseView.extend({
                 navigateToAnalyze();
             })
             .fail(function(message) {
-                displayAlert(message, modalModels.AlertTypes.warn);
+                displayAlert(message, modalModels.AlertTypes.error);
                 self.resetDrawingState();
             })
             .always(function() {
