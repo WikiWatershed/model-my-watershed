88a933ae2126630bf303cd83c5ecf3bd84857852
Terence Tuhinanshu <ttuhinanshu@azavea.com>
2017-08-02 16:25:06 -0400

    Add is_pageable model variable to Catalog
    This makes the code more scalable and standard rather than
    having a one-off check for CUAHSI.

--- src/mmw/js/src/data_catalog/views.js
+++ src/mmw/js/src/data_catalog/views.js
@@ -274,9 +274,7 @@ var TabContentView = Marionette.LayoutView.extend({
             model: this.model,
         }));
 
-        // CUAHSI does not support pagination, but others do
-        if (this.model.id !== 'cuahsi') {
-            this.resultRegion.$el.addClass('paginated');
+        if (this.model.get('is_pageable')) {
             this.pagerRegion.show(new PagerView({
                 model: this.model,
             }));
