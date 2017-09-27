f309bd9e2f3c99fa8e55dca9c89528c229ac4344
Alice Rottersman <arottersman@azavea.com>
2017-09-27 10:50:42 -0400

    Add migration to add client app user
    * Create a user for the client app so that it can get a token to send
    geoprocessing API requests

---
+++ src/mmw/apps/user/migrations/0003_client_app_user.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations
+from django.conf import settings
+from django.contrib.auth.models import User
+
+def add_client_app_user(apps, schema_editor):
+    web_app_user = User.objects.create_user(
+        username=settings.CLIENT_APP_USERNAME,
+        password=settings.CLIENT_APP_USER_PASSWORD)
+    web_app_user.save()
+
+
+def remove_client_app_user(apps, schema_editor):
+    User.objects.filter(username=settings.CLIENT_APP_USERNAME).delete()
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('authtoken', '0001_initial'),
+        ('user', '0001_initial'),
+        ('user', '0002_auth_tokens')
+    ]
+
+    operations = [
+        migrations.RunPython(add_client_app_user,
+                             remove_client_app_user)
+    ]
