18c59e05d09b46fec170c0af4a7e17a1576aadaa
Alice Rottersman <arottersman@azavea.com>
2017-10-02 17:51:45 -0400

    Remove spaces from Compare chart name
    * The name we were using for the chart svg ids had spaces in it, causing
      certain elements not work, such as the clipPath nvd3 creates
    
    * Remove spaces from the chart name so all charts have a clipPath

--- src/mmw/js/src/compare/views.js
+++ src/mmw/js/src/compare/views.js
@@ -353,6 +353,7 @@ var ChartRowView = Marionette.ItemView.extend({
             chartDiv = this.model.get('chartDiv'),
             chartEl = document.getElementById(chartDiv),
             name = this.model.get('name'),
+            chartName = name.replace(/\s/g, ''),
             label = this.model.get('unitLabel') +
                     ' (' + this.model.get('unit') + ')',
             colors = this.model.get('seriesColors'),
@@ -384,7 +385,7 @@ var ChartRowView = Marionette.ItemView.extend({
 
         $(chartEl.parentNode).css({ 'width': ((_.size(this.model.get('values')) * models.constants.COMPARE_COLUMN_WIDTH + models.constants.CHART_AXIS_WIDTH)  + 'px') });
         chart.renderCompareMultibarChart(
-            chartEl, name, label, colors, stacked, yMax, data,
+            chartEl, chartName, label, colors, stacked, yMax, data,
             models.constants.COMPARE_COLUMN_WIDTH, models.constants.CHART_AXIS_WIDTH, onRenderComplete);
     },
 });
