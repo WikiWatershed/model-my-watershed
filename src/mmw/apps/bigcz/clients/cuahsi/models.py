b3cb9fb1f4905b90b38b6a20148e39e4a6b05d06
Terence Tuhinanshu <ttuhinanshu@azavea.com>
2017-10-09 11:07:05 -0400

    Return more details for Variables
    Previously, a CUAHSI search would have a `concept_keywords` array
    of strings, corresponding to each variable. Now that we want to
    fetch more detailed values, we switch to having an array of
    `variables`, each of which in addition to `id`, `name` (that
    tends to be longer, more like a description than name), and
    `concept_keyword`, has a `site` and `wsdl` which will be used
    to fetch values for that variable in a given timespan.
    
    This array is sorted by `concept_keyword`, which will also be
    used as a display label in the UI.

--- src/mmw/apps/bigcz/clients/cuahsi/models.py
+++ src/mmw/apps/bigcz/clients/cuahsi/models.py
@@ -9,15 +9,16 @@ from apps.bigcz.models import Resource
 class CuahsiResource(Resource):
     def __init__(self, id, description, author, links, title,
                  created_at, updated_at, geom, details_url, sample_mediums,
-                 concept_keywords, service_org, service_code, service_url,
-                 service_title, service_citation, begin_date, end_date):
+                 variables, service_org, service_code, service_url,
+                 service_title, service_citation,
+                 begin_date, end_date):
         super(CuahsiResource, self).__init__(id, description, author, links,
                                              title, created_at, updated_at,
                                              geom)
 
         self.details_url = details_url
         self.sample_mediums = sample_mediums
-        self.concept_keywords = concept_keywords
+        self.variables = variables
         self.service_org = service_org
         self.service_code = service_code
         self.service_url = service_url
