801b46d8b818c405c0ed6b6655458eded27b7a7f
Alice Rottersman <arottersman@azavea.com>
2017-10-11 14:24:43 -0400

    Fix django user no-csrf-token test
    None of the user login tests were using a csrftoken (we assumed
    it was coming back from a `requests` request, but it wasn't). It
    appears the token wasn't ever being enforced, which led to us
    commenting out a test for the endpoint failing when the token
    wasn't there.
    
    * Remove ineffective fetch for csrf token
    * Modify attempt_login_without_token to use the Django test
      helper Client, which exposes an `enforce_csrf_checks` flag

--- src/mmw/apps/user/tests.py
+++ src/mmw/apps/user/tests.py
@@ -6,7 +6,8 @@ from __future__ import division
 import requests
 
 from django.test import LiveServerTestCase
-from django.test import TestCase
+from django.test import (TestCase,
+                         Client)
 from django.contrib.auth.models import User
 from apps.user.views import trim_to_valid_length
 
@@ -19,36 +20,18 @@ class TaskRunnerTestCase(LiveServerTestCase):
         User.objects.create_user(username='bob', email='bob@azavea.com',
                                  password='bob')
 
-    def get_token(self):
-        try:
-            init_response = requests.get(self.HOMEPAGE_URL)
-        except requests.RequestException:
-            init_response = {}
-
-        try:
-            csrf = init_response.cookies['csrftoken']
-        except KeyError:
-            csrf = None
-
-        return csrf
-
     def attempt_login(self, username, password):
-        csrf = self.get_token()
-        try:
-            headers = {'HTTP_X_CSRFTOKEN': csrf}
-            payload = {'username': username, 'password': password}
-            response = requests.post(self.LOGIN_URL, params=payload,
-                                     headers=headers)
-        except requests.RequestException:
-            response = {}
-        return response
-
-    def attempt_login_without_token(self, username, password):
         try:
             payload = {'username': username, 'password': password}
             response = requests.post(self.LOGIN_URL, params=payload)
         except requests.RequestException:
             response = {}
+        return response
+
+    def attempt_login_without_token(self, username, password):
+        c = Client(enforce_csrf_checks=True)
+        payload = {'username': username, 'password': password}
+        response = c.post(self.LOGIN_URL, params=payload)
         return response
 
     def test_no_username_returns_400(self):
@@ -87,12 +70,11 @@ class TaskRunnerTestCase(LiveServerTestCase):
                          'Incorrect server response. Expected 200 found %s'
                          % response.status_code)
 
-    # TODO: commented out because it fails and we don't know why yet.
-    # def test_no_token_good_credentials_returns_400(self):
-    #    response = self.attempt_login_without_token('bob', 'bob')
-    #    self.assertEqual(response.status_code, 400,
-    #                     'Incorrect server response. Expected 400 found %s'
-    #                     % response.status_code)
+    def test_no_token_good_credentials_returns_400(self):
+        response = self.attempt_login_without_token('bob', 'bob')
+        self.assertEqual(response.status_code, 400,
+                         'Incorrect server response. Expected 400 found %s'
+                         % response.status_code)
 
     def test_no_token_bad_credentials_returns_400(self):
         response = self.attempt_login_without_token('badbob', 'badpass')
