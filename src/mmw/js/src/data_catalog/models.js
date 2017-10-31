8c9ee6e139995a21736b2259bf40026ba833754f
Terence Tuhinanshu <ttuhinanshu@azavea.com>
2017-10-31 10:43:39 -0400

    Remove no data values from response
    Most of this diff is a rewrite to make it flow better, the only
    new thing being the addition of a response.variable.no_data_value
    check. If it exists, we filter out any such values from the
    response before adding it to the variable.

--- src/mmw/js/src/data_catalog/models.js
+++ src/mmw/js/src/data_catalog/models.js
@@ -752,24 +752,37 @@ var CuahsiVariable = Backbone.Model.extend({
     },
 
     parse: function(response) {
-        var mrv = null;
+        var values = this.get('values'),
+            ndv = response.variable.no_data_value,
+            newValues = response.values,
+            variable = {
+                name: response.variable.name,
+                data_type: response.variable.data_type,
+                sample_medium: response.variable.sample_medium,
+                units: response.variable.units.abbreviation,
+                most_recent_value: null,
+            };
 
-        if (response.values && response.values.length > 0) {
-            var values = this.get('values');
-
-            values.reset(response.values);
-            mrv = response.values[response.values.length - 1].value;
-        } else {
+        if (newValues === null || newValues === undefined || _.isEmpty(newValues)) {
             this.set('error', 'No values returned from API');
+            return variable;
         }
 
-        return {
-            name: response.variable.name,
-            data_type: response.variable.data_type,
-            sample_medium: response.variable.sample_medium,
-            units: response.variable.units.abbreviation,
-            most_recent_value: mrv,
-        };
+        if (ndv !== null && ndv !== undefined) {
+            newValues = _.filter(newValues, function(v) {
+                return v.value !== ndv;
+            });
+        }
+
+        if (_.isEmpty(newValues)) {
+            this.set('error', 'No valid values returned from API');
+            return variable;
+        }
+
+        values.reset(newValues);
+        variable.most_recent_value = _.last(newValues).value;
+
+        return variable;
     },
 
     getChartData: function() {
