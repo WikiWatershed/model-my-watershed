b465435a8aa3be325875a41d196288349b59359f
Matthew McFarland <mmcfarland@azavea.com>
2017-10-31 11:33:49 -0400

    Reset models affected by modification bug
    Resets the possibly affected multi-year model results by removing the
    calculated GIS parts, forcing a recomputation when accessed again.

---
+++ src/mmw/apps/modeling/migrations/0024_fix_gwlfe_gis_data.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations
+
+
+def fix_gis_data_serialization(apps, schema_editor):
+    """
+    Release 1.20.0 introduced a change which let the project "gis_data"
+    field get updated by modifications on a scenario.  This effectively
+    meant that modifications were being applied to all scenarios and that
+    removing them did not actually remove their effect from the gwlf-e
+    input.  For projects that were created and suffered from that bug,
+    clearing out the gis_data on Project and the results on Scenario
+    will force them to be recomputed with the fix applied.
+    """
+
+    Project = apps.get_model('modeling', 'Project')
+    bug_released_date = '2017-10-17'
+
+    # Apply fix to Multi-Year projects created after the release
+    for project in Project.objects.filter(created_at__gte=bug_released_date,
+                                          model_package='gwlfe'):
+        project.gis_data = None
+        for scenario in project.scenarios.all():
+            scenario.results = None
+            scenario.save()
+
+        project.save()
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('modeling', '0023_fix_gis_data_serialization'),
+    ]
+
+    operations = [
+        migrations.RunPython(fix_gis_data_serialization,
+                             migrations.RunPython.noop)
+    ]
