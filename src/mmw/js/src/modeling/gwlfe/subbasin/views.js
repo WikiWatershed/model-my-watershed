c75a317fa893404929dfb4e869dcf2f96baacc55
Alice Rottersman <arottersman@azavea.com>
2018-04-10 19:36:05 -0400

    Add AOI Name and Model Desc. To Subbasin View
    Add a header with title and description to the subbasin
    AOI level view.

--- src/mmw/js/src/modeling/gwlfe/subbasin/views.js
+++ src/mmw/js/src/modeling/gwlfe/subbasin/views.js
@@ -1,10 +1,37 @@
 "use strict";
 
 var Marionette = require('../../../../shim/backbone.marionette'),
+    App = require('../../../app'),
     resultTmpl = require('./templates/result.html');
 
 var ResultView = Marionette.LayoutView.extend({
     template: resultTmpl,
+
+    regions: {
+        tabRegion: '.subbasin-tab-region',
+    },
+
+    templateHelpers: function() {
+        return {
+            aoiDetails: App.currentProject.get('area_of_interest_name'),
+        };
+    },
+
+    onShow: function() {
+        this.tabRegion.show(new ResultTabs({}));
+    },
+});
+
+var ResultTabs = Marionette.CollectionView.extend({
+    tagName: 'ul',
+    className: 'nav nav-tabs model-nav-tabs',
+    attributes: {
+        role: 'tablist'
+    },
+
+    onRender: function() {
+        this.$el.find('li:first').addClass('active');
+    },
 });
 
 module.exports = {
