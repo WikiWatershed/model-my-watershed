8ed6c76de47ce5368dec71decbe665d672be97d4
lsetiawan <landungs@uw.edu>
2018-04-23 10:27:46 -0700

    Add usgswqp to catalog listing

--- src/mmw/apps/bigcz/clients/usgswqp/__init__.py
+++ src/mmw/apps/bigcz/clients/usgswqp/__init__.py
@@ -3,3 +3,12 @@ from __future__ import (absolute_import,
                         division,
                         print_function,
                         unicode_literals)
+
+from apps.bigcz.clients.usgswqp.models import USGSResource
+from apps.bigcz.clients.usgswqp.serializers import USGSResourceSerializer
+
+# Import catalog name and search function, so it can be exported from here
+from apps.bigcz.clients.usgswqp.search import CATALOG_NAME, search  # NOQA
+
+model = USGSResource
+serializer = USGSResourceSerializer
