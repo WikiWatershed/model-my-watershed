WikiWatershed/model-my-watershed

commit 02b5a7fb68bad4f6d0eafbd0643ab9c55e44c58c

Parse version number and show accordingly
For release and hotfix version, we show the version number and
link to the corresponding tag. For local dev environments we
show "Local". For other branches we show the git describe text.
In cases when anything else goes wrong, we show "Unknown".

Also add tests for this.

src/mmw/js/src/core/modals/views.js
@@ -18,7 +18,7 @@ var _ = require('lodash'),
     modalHydroShareTmpl = require('./templates/hydroShareExportModal.html'),
     modalAlertTmpl = require('./templates/alertModal.html'),
     modalIframeTmpl = require('./templates/iframeModal.html'),
-    vizerUrls = require('../settings').get('vizer_urls'),
+    settings = require('../settings'),
 
     ENTER_KEYCODE = 13,
     ESCAPE_KEYCODE = 27,
@@ -507,7 +507,7 @@ var PlotView = ModalBaseView.extend({
     plotMeasurement: function(varId) {
         var self = this,
             series = self.model.get('seriesMap')[varId],
-            dataUrl = vizerUrls.variable
+            dataUrl = settings.vizerUrls.variable
                .replace(/{{var_id}}/, varId)
                .replace(/{{asset_id}}/, this.model.get('siso_id'));
 
@@ -639,6 +639,12 @@ var AboutModal = Marionette.ItemView.extend({
     },
 
     template: modalAboutTmpl,
+    templateHelpers: function() {
+        return coreUtils.parseVersion(
+            settings.get('branch'),
+            settings.get('gitDescribe')
+        );
+    },
 
     onRender: function() {
         this.$el.modal('show');

src/mmw/js/src/core/modals/templates/aboutModal.html
@@ -6,9 +6,7 @@
             </button>
             <h2 class="modal-title">Model My Watershed</h2>
             <p class="version">
-                <!-- TODO Add Version Number Here -->
-                <!-- https://github.com/WikiWatershed/model-my-watershed/issues/2591 -->
-                Version X.XX (<a href="https://github.com/WikiWatershed/model-my-watershed/releases/latest">Release Notes</a>)
+                Version {{ version }} (<a href="{{ releaseNotesUrl }}">Release Notes</a>)
             </p>
         </div>
         <div class="modal-body">

src/mmw/js/src/core/tests.js
@@ -725,6 +725,37 @@ describe('Core', function() {
             assert.deepEqual(utils.rangeInMagnitude(10), {min: 0, max: 10});
             assert.deepEqual(utils.rangeInMagnitude(-0.005), {min: -0.01, max: 0});
         });
+
+        it('parses version numbers correctly', function() {
+            var latestUrl = 'https://github.com/WikiWatershed/model-my-watershed/releases/latest',
+                taggedUrl = 'https://github.com/WikiWatershed/model-my-watershed/releases/tag/1.22.0',
+                gitDescribe = '1.21.0-131-g9e49b30ce4f5b641e41c96f49c0a1509b21261b2',
+                output = null;
+
+            output = utils.parseVersion(null, null);
+            assert.equal(output.version, 'Unknown');
+            assert.equal(output.releaseNotesUrl, latestUrl);
+
+            output = utils.parseVersion('release/1.22.0', gitDescribe);
+            assert.equal(output.version, '1.22.0');
+            assert.equal(output.releaseNotesUrl, taggedUrl);
+
+            output = utils.parseVersion('hotfix/1.22.2', gitDescribe);
+            assert.equal(output.version, '1.22.2');
+            assert.equal(output.releaseNotesUrl, taggedUrl);
+
+            output = utils.parseVersion('develop', gitDescribe);
+            assert.equal(output.version, '1.21.0-131-g9e49b3');
+            assert.equal(output.releaseNotesUrl, latestUrl);
+
+            output = utils.parseVersion('tt/version-number', gitDescribe);
+            assert.equal(output.version, '1.21.0-131-g9e49b3');
+            assert.equal(output.releaseNotesUrl, latestUrl);
+
+            output = utils.parseVersion('local', null);
+            assert.equal(output.version, 'Local');
+            assert.equal(output.releaseNotesUrl, latestUrl);
+        });
     });
 });
 

src/mmw/js/src/core/utils.js
@@ -10,6 +10,8 @@ var L = require('leaflet'),
 
 var M2_IN_KM2 = 1000000;
 var noData = 'No Data';
+var RELEASE_NOTES_BASE_URL = 'https://github.com/WikiWatershed/model-my-watershed/releases';
+var MINOR_MAJOR_REGEX = /^[0-9]+\.[0-9]+\./; // Matches strings like 2.22.
 
 var utils = {
     layerGroupZIndices: {
@@ -606,6 +608,34 @@ var utils = {
         return {
             min: min,
             max: max
+        };
+    },
+
+    // Parses version number from branch and git describe output
+    // Outputs version number and release notes URL
+    parseVersion: function(branch, gitDescribe) {
+        var version = 'Unknown',
+            url = RELEASE_NOTES_BASE_URL + '/latest';
+
+        if (branch === null && gitDescribe === null) {
+            version = 'Unknown';
+        } else if (branch === 'local' && gitDescribe === null) {
+            version = 'Local';
+        } else if (branch.startsWith('release')) {
+            version = branch.substr(branch.indexOf('/') + 1);
+            // Use this version for release notes
+            url = RELEASE_NOTES_BASE_URL + '/tag/' + version;
+        } else if (branch.startsWith('hotfix')) {
+            version = branch.substr(branch.indexOf('/') + 1);
+            // Use the original release notes
+            url = RELEASE_NOTES_BASE_URL + '/tag/' + version.match(MINOR_MAJOR_REGEX) + '0';
+        } else {
+            version = gitDescribe.substr(0, 18);
+        }
+
+        return {
+            version: version,
+            releaseNotesUrl: url
         };
     }
 };

commit ef33a8487630e0a32aafd85c6cb2b95f6a2bb6c5

Fix Observations Layer tab data loading
This bug was introduced in 02b5a7fb. Allows the Observations
Layer tab to load historical data again.

src/mmw/js/src/core/modals/views.js
@@ -634,7 +634,7 @@ var PlotView = ModalBaseView.extend({
     plotMeasurement: function(varId) {
         var self = this,
             series = self.model.get('seriesMap')[varId],
-            dataUrl = settings.vizerUrls.variable
+            dataUrl = settings.get('vizer_urls').variable
                .replace(/{{var_id}}/, varId)
                .replace(/{{asset_id}}/, this.model.get('siso_id'));
 