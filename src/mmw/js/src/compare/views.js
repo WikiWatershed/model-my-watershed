7687a514b75ce3d5b0059db54df6f0af09b613ce
Terence Tuhinanshu <ttuhinanshu@azavea.com>
2018-08-13 15:16:14 -0400

    Add color badge to GWLFE Scenario
    We add a color out of a list of 32 to every scenario copied.
    This color will be used to highlight the item in the chart
    and also in the header row for GWLFE. If there are more than
    32 scenarios, we will start reusing the colors.
    
    Adding the color to a TR-55 scenario is effectively a no-op.

--- src/mmw/js/src/compare/views.js
+++ src/mmw/js/src/compare/views.js
@@ -31,7 +31,7 @@ var _ = require('lodash'),
     compareModificationsPopoverTmpl = require('./templates/compareModificationsPopover.html'),
     compareDescriptionPopoverTmpl = require('./templates/compareDescriptionPopover.html');
 
-var GWLFE_COLORS =  ['#3366cc','#dc3912','#ff9900','#109618','#990099',
+var SCENARIO_COLORS =  ['#3366cc','#dc3912','#ff9900','#109618','#990099',
         '#0099c6','#dd4477', '#66aa00','#b82e2e','#316395','#3366cc','#994499',
         '#22aa99','#aaaa11', '#6633cc','#e67300','#8b0707','#651067','#329262',
         '#5574a6','#3b3eac', '#b77322','#16d620','#b91383','#f4359e','#9c5935',
@@ -1006,7 +1006,7 @@ function getGwlfeTabs(scenarios) {
     ]);
 }
 
-function copyScenario(scenario, aoi_census) {
+function copyScenario(scenario, aoi_census, color) {
     var newScenario = new modelingModels.ScenarioModel({}),
         fetchResults = _.bind(newScenario.fetchResults, newScenario),
         debouncedFetchResults = _.debounce(fetchResults, 500);
@@ -1023,6 +1023,7 @@ function copyScenario(scenario, aoi_census) {
         inputmod_hash: scenario.get('inputmod_hash'),
         allow_save: false,
         active: scenario.get('active'),
+        color: color,
     });
 
     newScenario.get('inputs').on('add', debouncedFetchResults);
@@ -1052,8 +1053,9 @@ function getCompareScenarios(isTr55) {
         tempScenarios.add(forestScenario);
     }
 
-    trueScenarios.forEach(function(scenario) {
-        tempScenarios.add(copyScenario(scenario, aoi_census));
+    trueScenarios.forEach(function(scenario, index) {
+        var color = SCENARIO_COLORS[index % 32];
+        tempScenarios.add(copyScenario(scenario, aoi_census, color));
     });
 
     return tempScenarios;
