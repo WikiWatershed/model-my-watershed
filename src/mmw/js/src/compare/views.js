7dd671d8af60bd6e0eb6c8dfb07b16e96bc64697
Terence Tuhinanshu <ttuhinanshu@azavea.com>
2018-08-13 14:09:41 -0400

    Add static list of 32 colors for GWLFE Scenarios
    Refs #2912

--- src/mmw/js/src/compare/views.js
+++ src/mmw/js/src/compare/views.js
@@ -30,6 +30,12 @@ var _ = require('lodash'),
     compareModificationsPopoverTmpl = require('./templates/compareModificationsPopover.html'),
     compareDescriptionPopoverTmpl = require('./templates/compareDescriptionPopover.html');
 
+var GWLFE_COLORS =  ['#3366cc','#dc3912','#ff9900','#109618','#990099',
+        '#0099c6','#dd4477', '#66aa00','#b82e2e','#316395','#3366cc','#994499',
+        '#22aa99','#aaaa11', '#6633cc','#e67300','#8b0707','#651067','#329262',
+        '#5574a6','#3b3eac', '#b77322','#16d620','#b91383','#f4359e','#9c5935',
+        '#a9c413','#2a778d', '#668d1c','#bea413','#0c5922','#743411'];
+
 var CompareWindow2 = modalViews.ModalBaseView.extend({
     template: compareWindow2Tmpl,
 
