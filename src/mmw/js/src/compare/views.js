053867ae694f4218fc124d3abed17eba8c356f97
Kelly Innes <kinnes@azavea.com>
2018-08-27 13:36:47 -0400

    Adjust constant formatting

--- src/mmw/js/src/compare/views.js
+++ src/mmw/js/src/compare/views.js
@@ -33,12 +33,16 @@ var _ = require('lodash'),
     compareModificationsPopoverTmpl = require('./templates/compareModificationsPopover.html'),
     compareDescriptionPopoverTmpl = require('./templates/compareDescriptionPopover.html');
 
-var SCENARIO_COLORS =  ['#3366cc','#dc3912','#ff9900','#109618','#990099',
-        '#0099c6','#dd4477', '#66aa00','#b82e2e','#316395','#3366cc','#994499',
-        '#22aa99','#aaaa11', '#6633cc','#e67300','#8b0707','#651067','#329262',
-        '#5574a6','#3b3eac', '#b77322','#16d620','#b91383','#f4359e','#9c5935',
-        '#a9c413','#2a778d', '#668d1c','#bea413','#0c5922','#743411'],
-    monthNames = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+var SCENARIO_COLORS =  [
+        '#3366cc','#dc3912','#ff9900','#109618','#990099', '#0099c6', '#dd4477',
+        '#66aa00','#b82e2e','#316395','#3366cc','#994499', '#22aa99','#aaaa11',
+        '#6633cc','#e67300','#8b0707','#651067','#329262', '#5574a6','#3b3eac',
+        '#b77322','#16d620','#b91383','#f4359e','#9c5935', '#a9c413','#2a778d',
+        '#668d1c','#bea413','#0c5922','#743411'
+    ],
+    monthNames = [
+        'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'
+    ],
     hydrologyKeys = Object.freeze({
         streamFlow: 'AvStreamFlow',
         surfaceRunoff: 'AvRunoff',
@@ -47,7 +51,6 @@ var SCENARIO_COLORS =  ['#3366cc','#dc3912','#ff9900','#109618','#990099',
         evapotranspiration: 'AvEvapoTrans',
         precipitation: 'AvPrecipitation',
     });
-
 
 var CompareModal = modalViews.ModalBaseView.extend({
     template: compareModalTmpl,
