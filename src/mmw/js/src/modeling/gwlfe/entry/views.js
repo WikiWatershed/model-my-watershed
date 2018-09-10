5457395c4b340d7b84a4506df51c3e4e948983ff
Terence Tuhinanshu <ttuhinanshu@azavea.com>
2018-09-10 11:44:41 -0400

    Add models, views, template for tab contents
    Also namespace tabs with `#entry_` prefix to keep them separate
    from other tabs that may be visible with the same name.

--- src/mmw/js/src/modeling/gwlfe/entry/views.js
+++ src/mmw/js/src/modeling/gwlfe/entry/views.js
@@ -3,6 +3,7 @@
 var Marionette = require('../../../../shim/backbone.marionette'),
     modalViews = require('../../../core/modals/views'),
     modalTmpl = require('./templates/modal.html'),
+    tabContentTmpl = require('./templates/tabContent.html'),
     tabPanelTmpl = require('./templates/tabPanel.html');
 
 var EntryModal = modalViews.ModalBaseView.extend({
@@ -13,12 +14,18 @@ var EntryModal = modalViews.ModalBaseView.extend({
 
     regions: {
         panelsRegion: '.tab-panels-region',
+        contentsRegion: '.tab-contents-region',
     },
 
     // Override to populate tabs
     onRender: function() {
+        var tabs = this.model.get('tabs');
+
         this.panelsRegion.show(new TabPanelsView({
-            collection: this.model.get('tabs'),
+            collection: tabs,
+        }));
+        this.contentsRegion.show(new TabContentsView({
+            collection: tabs,
         }));
 
         this.$el.modal('show');
@@ -44,6 +51,28 @@ var TabPanelsView = Marionette.CollectionView.extend({
 
     onRender: function() {
         this.$('li:first').addClass('active');
+    },
+});
+
+var TabContentView = Marionette.LayoutView.extend({
+    className: 'tab-pane',
+    template: tabContentTmpl,
+    attributes: {
+        role: 'tabpanel'
+    },
+
+    id: function() {
+        return 'entry_' + this.model.get('name');
+    },
+});
+
+var TabContentsView = Marionette.CollectionView.extend({
+    className: 'tab-content',
+
+    childView: TabContentView,
+
+    onRender: function() {
+        this.$('.tab-pane:first').addClass('active');
     },
 });
 
