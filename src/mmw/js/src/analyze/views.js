f2a67e793501d3ee1b4a94c17b8ecb9914332897
Terence Tuhinanshu <ttuhinanshu@azavea.com>
2018-09-28 13:35:39 -0400

    Remove unused components

--- src/mmw/js/src/analyze/views.js
+++ src/mmw/js/src/analyze/views.js
@@ -46,117 +46,10 @@ var $ = require('jquery'),
     tabPanelTmpl = require('../modeling/templates/resultsTabPanel.html'),
     tabContentTmpl = require('./templates/tabContent.html'),
     barChartTmpl = require('../core/templates/barChart.html'),
-    resultsWindowTmpl = require('./templates/resultsWindow.html'),
-    modelSelectionDropdownTmpl = require('./templates/modelSelectionDropdown.html');
+    resultsWindowTmpl = require('./templates/resultsWindow.html');
 
 var monthNames = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
                   'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
-
-var ModelSelectionDropdownView = Marionette.ItemView.extend({
-    template: modelSelectionDropdownTmpl,
-
-    ui: {
-        'modelPackageLinks': 'a.analyze-model-package-link',
-    },
-
-    events: {
-        'click @ui.modelPackageLinks': 'selectModelPackage',
-    },
-
-    selectModelPackage: function(e) {
-        e.preventDefault();
-
-        var modelPackages = settings.get('model_packages'),
-            modelPackageName = $(e.currentTarget).data('id'),
-            modelPackage = lodash.find(modelPackages, {name: modelPackageName}),
-            newProjectUrl = '/project/new/' + modelPackageName,
-            projectUrl = '/project',
-            alertView,
-            aoiModel = new coreModels.GeoModel({
-                shape: App.map.get('areaOfInterest'),
-                place: App.map.get('areaOfInterestName')
-            }),
-            analysisResults = App.getAnalyzeCollection()
-                                  .findWhere({taskName: 'analyze/land'})
-                                  .get('result') || {},
-            landResults = analysisResults.survey;
-
-        if (modelPackageName === utils.GWLFE && settings.get('mapshed_max_area')) {
-            var areaInSqKm = utils.changeOfAreaUnits(aoiModel.get('area'),
-                                                     aoiModel.get('units'),
-                                                     'km<sup>2</sup>'),
-                mapshedMaxArea = settings.get('mapshed_max_area');
-
-            if (areaInSqKm > mapshedMaxArea) {
-                alertView = new modalViews.AlertView({
-                    model: new modalModels.AlertModel({
-                        alertMessage: "The selected Area of Interest is too big for " +
-                                 "the Watershed Multi-Year Model. The currently " +
-                                 "maximum supported size is " + mapshedMaxArea + " km².",
-                        alertType: modalModels.AlertTypes.warn
-                    })
-                });
-                alertView.render();
-                return;
-            }
-        }
-
-        if (landResults) {
-            var landCoverTotal = lodash.sum(lodash.map(landResults.categories,
-                    function(category) {
-                        if (category.type === 'Open Water') {
-                            return 0;
-                        }
-                        return category.area;
-                    }));
-
-            if (landCoverTotal === 0) {
-                alertView = new modalViews.AlertView({
-                    model: new modalModels.AlertModel({
-                        alertMessage: "The selected Area of Interest doesn't " +
-                                 "include any land cover to run the model.",
-                        alertType: modalModels.AlertTypes.warn
-                    })
-                });
-
-                alertView.render();
-                return;
-            }
-        }
-
-        if (!modelPackage.disabled) {
-            if (settings.get('itsi_embed') && App.currentProject && !App.currentProject.get('needs_reset')) {
-                var currModelPackageName = App.currentProject.get('model_package');
-                if (modelPackageName === currModelPackageName) {
-                    // Go to existing project
-                    router.navigate(projectUrl, {trigger: true});
-                } else {
-                    var confirmNewProject = new modalViews.ConfirmView({
-                        model: new modalModels.ConfirmModel({
-                            question: 'If you change the model you will lose your current work.',
-                            confirmLabel: 'Switch Model',
-                            cancelLabel: 'Cancel',
-                            feedbackRequired: true
-                        }),
-                    });
-
-                    confirmNewProject.on('confirmation', function() {
-                        router.navigate(newProjectUrl, {trigger: true});
-                    });
-                    confirmNewProject.render();
-                }
-            } else {
-                router.navigate(newProjectUrl, {trigger: true});
-            }
-        }
-    },
-
-    templateHelpers: function() {
-        return {
-            modelPackages: settings.get('model_packages')
-        };
-    },
-});
 
 var ResultsView = Marionette.LayoutView.extend({
     id: 'model-output-wrapper',
@@ -180,7 +73,6 @@ var ResultsView = Marionette.LayoutView.extend({
         aoiRegion: '.aoi-region',
         analyzeRegion: '#analyze-tab-contents',
         monitorRegion: '#monitor-tab-contents',
-        nextStageRegion: '#next-stage-navigation-region',
     },
 
     templateHelpers: {
@@ -191,9 +83,6 @@ var ResultsView = Marionette.LayoutView.extend({
     onShow: function() {
         this.showAoiRegion();
         this.showDetailsRegion();
-        if (!settings.get('data_catalog_enabled')) {
-            this.showModelSelectionDropdown();
-        }
     },
 
     onRender: function() {
@@ -217,10 +106,6 @@ var ResultsView = Marionette.LayoutView.extend({
         this.monitorRegion.show(new dataCatalogViews.DataCatalogWindow(
             App.getDataCatalog()
         ));
-    },
-
-    showModelSelectionDropdown: function() {
-        this.nextStageRegion.show(new ModelSelectionDropdownView());
     },
 
     changeArea: function() {
