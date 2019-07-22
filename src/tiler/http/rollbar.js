aaf34b2e98c2bc07f717a712a4f1e26a500e55d0
Terence Tuhinanshu <ttuhinanshu@azavea.com>
2019-07-22 13:46:42 -0400

    Report correct environment in Rollbar
    Previously, because Rollbar wasn't initialized with an
    environment, it would always default to "development".
    With proper initialization, it reports the correct one
    for development, staging, and production.

--- src/tiler/http/rollbar.js
+++ src/tiler/http/rollbar.js
@@ -1,9 +1,13 @@
 'use strict';
 
 var Rollbar = require('rollbar'),
-    token = process.env.ROLLBAR_SERVER_SIDE_ACCESS_TOKEN;
+    token = process.env.ROLLBAR_SERVER_SIDE_ACCESS_TOKEN,
+    stackType = process.env.MMW_STACK_TYPE;
 
-module.exports = token ? new Rollbar(token) : {
+module.exports = token ? new Rollbar({
+        accessToken: token,
+        environment: stackType,
+    }) : {
     handleError: ex => { console.error(ex); },
     errorHandler: () => {},
 };
