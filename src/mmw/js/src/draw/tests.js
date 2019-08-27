WikiWatershed/model-my-watershed

commit 6ac8212adcd118b339091e86b056809371545b9e

Upgrade Lodash to 4.17.15
This is a big upgrade, unfortuantely necessitated by
security fixes. This upgrade deprecates a number of
Underscore compatible aliases that Lodash supported,
requiring us to rename functions to be more Lodash
compatible.

This migration was largely informed by the instruction
here: https://github.com/lodash/lodash/wiki/Migrating

Also, in cases when both Underscore and Lodash were
used in the same file, we switch to only using Lodash.

If we want to replace Underscore completely and use
Lodash only, we'll have to upgrade Backbone and all
the related libraries. That is a much bigger lift,
and is deferred for a later date.

Also add Lodash as an explicit dependency in the tiler
to upgrade some transitive dependencies.

src/mmw/js/src/draw/tests.js
@@ -338,7 +338,7 @@ function assertDrawToolIsVisible($el, drawToolRegion) {
         assert.equal($el.find(drawToolRegion + ' .draw-tool-selection').length, 1);
     }
 
-    _.each(inactiveRegions, function (inactiveDrawToolRegion) {
+    _.forEach(inactiveRegions, function (inactiveDrawToolRegion) {
         assert.equal($el.find(inactiveDrawToolRegion + ' .draw-tool-region').length, 0);
     });
 }

src/mmw/js/src/draw/utils.js
@@ -168,7 +168,7 @@ function isSelfIntersecting(shape) {
         // valid for geoprocessing.
         // Assert only that each polygon ring doesn't intersect
         // itself
-        return _.any(geom.coordinates, function(linearRing) {
+        return _.some(geom.coordinates, function(linearRing) {
             return selfIntersects({
                 type: "Polygon",
                 coordinates: [linearRing],

src/mmw/js/src/draw/views.js
@@ -141,7 +141,7 @@ function validatePointWithinDataSourceBounds(latlng, dataSource) {
     switch (dataSource) {
         case utils.DRB:
             var streamLayers = settings.get('stream_layers');
-            perimeter = _.findWhere(streamLayers, {code: 'drb_streams_v2'}).perimeter;
+            perimeter = _.find(streamLayers, {code: 'drb_streams_v2'}).perimeter;
             point_outside_message = 'Selected point is outside the Delaware River Basin';
             break;
         case utils.NHD:

src/mmw/js/src/analyze/tests.js
@@ -56,7 +56,7 @@ describe('Analyze', function() {
 
     describe('Views', function() {
         describe('AnalysisResultView', function() {
-            _.each(['land', 'soil', 'animals', 'pointsource', 'catchment_water_quality'], testAnalysisType);
+            _.forEach(['land', 'soil', 'animals', 'pointsource', 'catchment_water_quality'], testAnalysisType);
         });
     });
 });

src/mmw/js/src/analyze/views.js
@@ -1,8 +1,7 @@
 "use strict";
 
 var $ = require('jquery'),
-    _ = require('underscore'),
-    lodash = require('lodash'),
+    _ = require('lodash'),
     L = require('leaflet'),
     Marionette = require('../../shim/backbone.marionette'),
     Backbone = require('../../shim/backbone'),
@@ -123,7 +122,7 @@ var ResultsView = Marionette.LayoutView.extend({
 
     animateIn: function(fitToBounds) {
         var self = this,
-            fit = lodash.isUndefined(fitToBounds) ? true : fitToBounds;
+            fit = _.isUndefined(fitToBounds) ? true : fitToBounds;
 
         this.$el.animate({ width: '400px', opacity: 1 }, 200, function() {
             App.map.setAnalyzeSize(fit);
@@ -161,7 +160,7 @@ var ResultsView = Marionette.LayoutView.extend({
 
         var modelPackages = settings.get('model_packages'),
             modelPackageName = $(e.currentTarget).data('id'),
-            modelPackage = lodash.find(modelPackages, {name: modelPackageName}),
+            modelPackage = _.find(modelPackages, {name: modelPackageName}),
             newProjectUrl = '/project/new/' + modelPackageName,
             projectUrl = '/project',
             alertView,
@@ -197,7 +196,7 @@ var ResultsView = Marionette.LayoutView.extend({
         }
 
         if (landResults) {
-            var landCoverTotal = lodash.sum(lodash.map(landResults.categories,
+            var landCoverTotal = _.sum(_.map(landResults.categories,
                 function(category) {
                     if (category.type === 'Open Water') {
                         return 0;
@@ -330,8 +329,8 @@ var TabContentView = Marionette.LayoutView.extend({
         this.showAnalyzingMessage();
 
         this.model.fetchAnalysisIfNeeded()
-            .done(lodash.bind(this.showResultsIfNotDestroyed, this))
-            .fail(lodash.bind(this.showErrorIfNotDestroyed, this));
+            .done(_.bind(this.showResultsIfNotDestroyed, this))
+            .fail(_.bind(this.showErrorIfNotDestroyed, this));
     },
 
     showAnalyzingMessage: function() {
@@ -698,8 +697,8 @@ var ClimateTableView = Marionette.CompositeView.extend({
     templateHelpers: function() {
         var data = this.collection.toJSON(),
             // Standardize precipitation in cm to m before converting to appropriate unit
-            totalPpt = coreUnits.get('LENGTH_S', lodash(data).pluck('ppt').sum() / 100),
-            avgTmean = coreUnits.get('TEMPERATURE', lodash(data).pluck('tmean').sum() / 12);
+            totalPpt = coreUnits.get('LENGTH_S', _(data).map('ppt').sum() / 100),
+            avgTmean = coreUnits.get('TEMPERATURE', _(data).map('tmean').sum() / 12);
 
         return {
             totalPpt: totalPpt.value,
@@ -766,14 +765,14 @@ var StreamTableView = Marionette.CompositeView.extend({
     },
 
     templateHelpers: function() {
-        var data = lodash(this.collection.toJSON()),
+        var data = _(this.collection.toJSON()),
             scheme = settings.get('unit_scheme'),
             lengthUnit = coreUnits[scheme].LENGTH_XL.name,
             isMetric = scheme === coreUnits.UNIT_SCHEME.METRIC,
             totalLength = isMetric ?
-                data.pluck('lengthkm').sum() :
-                data.pluck('lengthkm').sum() * MI_IN_KM,
-            avgChannelSlope = data.pluck('total_weighted_slope').sum() / totalLength,
+                data.map('lengthkm').sum() :
+                data.map('lengthkm').sum() * MI_IN_KM,
+            avgChannelSlope = data.map('total_weighted_slope').sum() / totalLength,
             // Currently we only calculate agricultral percent for the entire
             // set, not per stream order, so we use the first value for total.
             agPercent = data.first().ag_stream_pct,
@@ -1175,7 +1174,7 @@ var ChartView = Marionette.ItemView.extend({
     addChart: function() {
         var self = this,
             chartEl = this.$el.find('.bar-chart').get(0),
-            data = lodash.map(this.collection.toJSON(), function(model) {
+            data = _.map(this.collection.toJSON(), function(model) {
                 return {
                     x: model.type,
                     y: model.coverage,
@@ -1186,7 +1185,7 @@ var ChartView = Marionette.ItemView.extend({
             chartOptions = {
                yAxisLabel: 'Coverage',
                isPercentage: true,
-               barClasses: lodash.pluck(data, 'class')
+               barClasses: _.map(data, 'class')
            };
 
         chart.renderHorizontalBarChart(chartEl, data, chartOptions);
@@ -1221,7 +1220,7 @@ var AnalyzeResultView = Marionette.LayoutView.extend({
 
         // Render an unpaginated table for tables that can be paginated.
         if (dataName === 'pointsource' || dataName === 'catchment_water_quality') {
-            var largestArea = lodash.max(lodash.pluck(data, 'area')),
+            var largestArea = _.max(_.map(data, 'area')),
                 units = utils.magnitudeOfArea(largestArea),
                 census,
                 TableView;
@@ -1252,7 +1251,7 @@ var AnalyzeResultView = Marionette.LayoutView.extend({
     showAnalyzeResults: function(CategoriesToCensus, AnalyzeTableView,
         AnalyzeChartView, title, source, helpText, associatedLayerCodes, pageSize) {
         var categories = this.model.get('categories'),
-            largestArea = lodash.max(lodash.pluck(categories, 'area')),
+            largestArea = _.max(_.map(categories, 'area')),
             units = utils.magnitudeOfArea(largestArea),
             census = new CategoriesToCensus(categories);
 
@@ -1442,7 +1441,7 @@ var ClimateChartView = ChartView.extend({
                 xAxisLabel: function(x) {
                     return monthNames[x];
                 },
-                xTickValues: lodash.range(12),
+                xTickValues: _.range(12),
                 yTickFormat: '0.01f',
             };
 
@@ -1494,8 +1493,8 @@ var StreamResultView = AnalyzeResultView.extend({
             streamOrders = this.model.get('categories');
 
         // Merge helptext in with stream order results
-        _.each(streamOrderHelpText, function(help) {
-            var stream = _.findWhere(streamOrders, { order: help.order });
+        _.forEach(streamOrderHelpText, function(help) {
+            var stream = _.find(streamOrders, { order: help.order });
             if (stream) {
                 stream.helpText = help.text;
             }

src/mmw/js/src/core/modals/views.js
@@ -603,7 +603,7 @@ var PlotView = ModalBaseView.extend({
         this.listenTo(this.model, 'change', this.render);
 
         // Cache series data after it is fetched.
-        var seriesMap = _.object(_.map(this.model.get('measurements'), function(measurement) {
+        var seriesMap = _.fromPairs(_.map(this.model.get('measurements'), function(measurement) {
             return [measurement.var_id, null];
         }));
         this.model.set('seriesMap', seriesMap);
@@ -749,7 +749,7 @@ var PlotView = ModalBaseView.extend({
 
     templateHelpers: function() {
         var currVarId = this.model.get('currVarId'),
-            measurement = _.findWhere(this.model.get('measurements'), {
+            measurement = _.find(this.model.get('measurements'), {
                 var_id: currVarId
             }),
             series = this.model.get('seriesMap')[currVarId],

src/mmw/js/src/core/tests.js
@@ -204,7 +204,7 @@ describe('Core', function() {
                     }).get();
 
                 assert.equal(layerNames.length, 2, 'Did not add layer selector');
-                assert.deepEqual(layerNames, _.pluck(baseLayers, 'display'));
+                assert.deepEqual(layerNames, _.map(baseLayers, 'display'));
 
                 view.destroy();
             });
@@ -636,7 +636,7 @@ describe('Core', function() {
                 'TRACE'
             ];
 
-            _.each(requestMethods, function(method) {
+            _.forEach(requestMethods, function(method) {
                 $.ajax({
                     method: method,
                     url: '/foo',
@@ -658,7 +658,7 @@ describe('Core', function() {
                 'DELETE'
             ];
 
-            _.each(requestMethods, function(method) {
+            _.forEach(requestMethods, function(method) {
                 $.ajax({
                     method: method,
                     url: '/foo',
@@ -686,7 +686,7 @@ describe('Core', function() {
                 'PATCH'
             ];
 
-            _.each(requestMethods, function(method) {
+            _.forEach(requestMethods, function(method) {
                 $.ajax({
                     method: method,
                     url: 'http://www.example.com',
@@ -714,7 +714,7 @@ describe('Core', function() {
                 'PATCH'
             ];
 
-            _.each(requestMethods, function(method) {
+            _.forEach(requestMethods, function(method) {
                 $.ajax({
                     method: method,
                     url: 'https://www.secure-example.com',

src/mmw/js/src/core/utils.js
@@ -1,8 +1,7 @@
 "use strict";
 
 var L = require('leaflet'),
-    _ = require('underscore'),
-    lodash = require('lodash'),
+    _ = require('lodash'),
     md5 = require('blueimp-md5').md5,
     intersect = require('turf-intersect'),
     centroid = require('turf-centroid'),
@@ -314,7 +313,7 @@ var utils = {
     parseQueryString: function(queryString) {
         var params = {};
         if (queryString) {
-            _.each(
+            _.forEach(
                 _.map(decodeURI(queryString).split(/&/g), function(el) {
                     var aux = el.split('='), o = {};
                     if (aux.length >= 1) {
@@ -475,13 +474,13 @@ var utils = {
     },
 
     totalForPointSourceCollection: function(collection, key) {
-        return lodash.sum(lodash.map(collection, function(element) {
+        return _.sum(_.map(collection, function(element) {
             return element.attributes[key] || 0;
         }));
     },
 
     geomForIdInCatchmentWaterQualityCollection: function(collection, key, id) {
-        return lodash.find(collection, function(element) {
+        return _.find(collection, function(element) {
             return element.attributes[key] === id;
         }).attributes['geom'] || null;
     },
@@ -541,8 +540,8 @@ var utils = {
     isWKAoIValid: function(wkaoi) {
         return wkaoi &&
                wkaoi.includes('__') &&
-               !lodash.startsWith(wkaoi, '__') &&
-               !lodash.endsWith(wkaoi, '__');
+               !_.startsWith(wkaoi, '__') &&
+               !_.endsWith(wkaoi, '__');
     },
 
     isWKAoIValidForSubbasinModeling: function(wkaoi) {
@@ -561,7 +560,7 @@ var utils = {
 
     isInDrb: function(geom) {
         var layers = settings.get('stream_layers'),
-            drb = _.findWhere(layers, {code: 'drb_streams_v2'}).perimeter;
+            drb = _.find(layers, {code: 'drb_streams_v2'}).perimeter;
 
         return !!intersect(geom, drb);
     },

commit 096d84e97fe1a886fbfd5e80809207c534b7b1e3

Isolate failing code and add failing test
The bit of code that generates a 1 square km box from a
given point is isolated into a utility function so it can
be tested separately. Also add a test that exercises the
code with sample values taken from production (where the
failure does not occur).

The failure is likely due to a subtle change in Lodash
behavior caused by the upgrade in 6ac8212a.

src/mmw/js/src/draw/tests.js
@@ -63,6 +63,28 @@ var sandboxId = 'sandbox',
     TEST_FEATURECOLLECTION = {
         'type': 'FeatureCollection',
         'features': [TEST_FEATURE],
+    },
+    TEST_POINT = {
+        "type": "Feature",
+        "properties": {},
+        "geometry": {
+            "type": "Point",
+            "coordinates": [-75.180344581604, 39.9603954838161]
+        }
+    },
+    TEST_1KM_BOX = {
+        "type": "Feature",
+        "geometry": {
+            "type": "Polygon",
+            "coordinates": [[
+                [-75.18620924421529, 39.955900286929776],
+                [-75.17447991899269, 39.955900286929776],
+                [-75.17447991899269, 39.96489068070241],
+                [-75.18620924421529, 39.96489068070241],
+                [-75.18620924421529, 39.955900286929776]
+            ]]
+        },
+        "properties": {}
     };
 
 var SandboxRegion = Marionette.Region.extend({
@@ -276,6 +298,13 @@ describe('Draw', function() {
 
             assert.equal(isSelfIntersecting(TEST_SELF_INTERSECTING_POLYGON), true,
                          'did not find intersections in intersecting polygon');
+        });
+
+        it('calculates 1km square box around a point', function() {
+            var box = utils.getSquareKmBoxForPoint(TEST_POINT);
+
+            assert.deepEqual(TEST_1KM_BOX, box,
+                             'did not generate 1km square box correctly');
         });
     });
 });

src/mmw/js/src/draw/utils.js
@@ -6,6 +6,7 @@ var $ = require('jquery'),
     JSZip = require('jszip'),
     turfArea = require('turf-area'),
     turfBboxPolygon = require('turf-bbox-polygon'),
+    turfDestination = require('turf-destination'),
     turfKinks = require('turf-kinks'),
     intersect = require('turf-intersect'),
     settings = require('../core/settings');
@@ -124,6 +125,33 @@ function shapeBoundingBoxArea(shape) {
     return turfArea(boundingBoxPolygon);
 }
 
+// Given a point, returns a 1 KM Square bounding box around it
+function getSquareKmBoxForPoint(point) {
+    var halfKmbufferPoints = _.map([-180, -90, 0, 90], function(bearing) {
+            var p = turfDestination(point, 0.5, bearing, 'kilometers');
+            return L.latLng(p.geometry.coordinates[1], p.geometry.coordinates[0]);
+        }),
+        // Convert the four points into two SW and NE for the bounding
+        // box. Do this by splitting the array into two arrays of two
+        // points. Then map each array of two to a single point by
+        // taking the lat from one and lng from the other.
+        swNe = _.map(_.toArray(_.groupBy(halfKmbufferPoints, function(p, i) {
+            // split the array of four in half.
+            return i < 2;
+        })), function(pointGroup) {
+            return L.latLng(pointGroup[0].lat, pointGroup[1].lng);
+        }),
+        bounds = L.latLngBounds(swNe),
+        box = turfBboxPolygon(bounds.toBBoxString().split(','));
+
+    // Convert coordinates from using strings to floats so that backend can parse them.
+    box.geometry.coordinates[0] = _.map(box.geometry.coordinates[0], function(coord) {
+        return [parseFloat(coord[0]), parseFloat(coord[1])];
+    });
+
+    return box;
+}
+
 function getFileFromZipObjects(zipObjects, extension) {
     return _.find(zipObjects.files, function(zipObject) {
         var fileExtension = zipObject.name.substr(zipObject.name.lastIndexOf(".") + 1);
@@ -234,6 +262,7 @@ module.exports = {
     isValidForAnalysis: isValidForAnalysis,
     withinConus: withinConus,
     getPolygonFromGeoJson: getPolygonFromGeoJson,
+    getSquareKmBoxForPoint: getSquareKmBoxForPoint,
     loadAsyncShpFilesFromZip: loadAsyncShpFilesFromZip,
     NHD: 'nhd',
     DRB: 'drb',

src/mmw/js/src/draw/views.js
@@ -5,8 +5,6 @@ var $ = require('jquery'),
     JSZip = require('jszip'),
     L = require('leaflet'),
     Marionette = require('../../shim/backbone.marionette'),
-    turfBboxPolygon = require('turf-bbox-polygon'),
-    turfDestination = require('turf-destination'),
     turfIntersect = require('turf-intersect'),
     shapefile = require('shapefile'),
     reproject = require('reproject'),
@@ -834,27 +832,7 @@ var DrawAreaView = DrawToolBaseView.extend({
 
         utils.placeMarker(map).then(function(latlng) {
             var point = L.marker(latlng).toGeoJSON(),
-                halfKmbufferPoints = _.map([-180, -90, 0, 90], function(bearing) {
-                    var p = turfDestination(point, 0.5, bearing, 'kilometers');
-                    return L.latLng(p.geometry.coordinates[1], p.geometry.coordinates[0]);
-                }),
-                // Convert the four points into two SW and NE for the bounding
-                // box. Do this by splitting the array into two arrays of two
-                // points. Then map each array of two to a single point by
-                // taking the lat from one and lng from the other.
-                swNe = _.map(_.toArray(_.groupBy(halfKmbufferPoints, function(p, i) {
-                    // split the array of four in half.
-                    return i < 2;
-                })), function(pointGroup) {
-                    return L.latLng(pointGroup[0].lat, pointGroup[1].lng);
-                }),
-                bounds = L.latLngBounds(swNe),
-                box = turfBboxPolygon(bounds.toBBoxString().split(','));
-
-            // Convert coordinates from using strings to floats so that backend can parse them.
-            box.geometry.coordinates[0] = _.map(box.geometry.coordinates[0], function(coord) {
-                return [parseFloat(coord[0]), parseFloat(coord[1])];
-            });
+                box = utils.getSquareKmBoxForPoint(point);
 
             window.ga('send', 'event', GA_AOI_CATEGORY, 'aoi-create', 'squarekm');
             return box;