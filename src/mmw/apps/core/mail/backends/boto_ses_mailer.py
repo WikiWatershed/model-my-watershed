a8603d7c143e8bd42e9075e689faed6fd96cf945
Terence Tuhinanshu <ttuhinanshu@azavea.com>
2021-11-15 17:14:31 -0500

    Replace maxint with maxsize

--- src/mmw/apps/core/mail/backends/boto_ses_mailer.py
+++ src/mmw/apps/core/mail/backends/boto_ses_mailer.py
@@ -31,7 +31,7 @@ class EmailBackend(BaseEmailBackend):
         if self.check_quota:
             remaining_quota = self.mailer.get_remaining_message_quota()
         else:
-            remaining_quota = sys.maxint
+            remaining_quota = sys.maxsize
 
         if len(email_messages) <= remaining_quota:
             for email_message in email_messages:
