e9e1a32ba3c183cab5d2d34cb6c905434c281a6a
Terence Tuhinanshu <terence@tuhinanshu.com>
2023-06-16 19:27:21 +0000

    Replace ga.send with dataLayer.push
    Instead of sending custom events to Google Analytics, Tag Manager
    expects us to add them to a dataLayer managed in the browser,
    from where it can pull them on demand.
    
    These are tagged with `mmw`, which must also be configured in
    Google Tag Manager to work properly.

--- src/mmw/js/src/analyze/models.js
+++ src/mmw/js/src/analyze/models.js
@@ -98,7 +98,8 @@ var AnalyzeTaskModel = coreModels.TaskModel.extend({
             var gaEvent = self.get('name') + '-analyze',
                 gaLabel = utils.isInDrb(aoi) ? 'drb-aoi' : 'national-aoi',
                 gaAoiSize = turfArea(aoi) / 1000000;
-            window.ga('send', 'event', 'Analyze', gaEvent, gaLabel, parseInt(gaAoiSize));
+            
+            utils.gtm('Analyze', gaEvent, gaLabel, parseInt(gaAoiSize));
 
             var isWkaoi = utils.isWKAoIValid(wkaoi),
                 taskHelper = {
