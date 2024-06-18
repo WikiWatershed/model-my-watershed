4f046259ac700866b2e60a7067e679abb5bd164a
Terence Tuhinanshu <terence@tuhinanshu.com>
2024-06-18 17:04:43 -0400

    Add stream-order filtered demo tileset

--- src/mmw/mmw/settings/layer_settings.py
+++ src/mmw/mmw/settings/layer_settings.py
@@ -626,6 +626,15 @@ LAYER_GROUPS = {
             'vectorType': True,
             'url': 'https://{s}.tiles.azavea.com/TDX_streamnet_7020038340_02/{z}/{x}/{y}.pbf',  # NOQA
         },
+        {
+            'code': 'TDX_streamnet_7020038340_01',
+            'display': 'TDX Streamnet 7020038340 - Ordered',
+            'minZoom': 0,
+            'maxNativeZoom': 13,
+            'maxZoom': 18,
+            'vectorType': True,
+            'url': 'https://{s}.tiles.azavea.com/TDX_streamnet_7020038340_01/{z}/{x}/{y}.pbf',  # NOQA
+        },
     ],
 }
 
