25bc332adaa9cbd75b9700ae513be13fe1dd508d
Terence Tuhinanshu <terence@tuhinanshu.com>
2024-07-02 21:17:58 -0400

    Interpret subprocess output as strings
    Previously the subprocess output would be reported as byte strings,
    which in Python 3.10 would add a b'' prefix, interfering with the
    output. By interpreting them as proper UTF-8 strings, we ensure
    that the original functionality is restored.

--- deployment/packer/driver.py
+++ deployment/packer/driver.py
@@ -59,7 +59,9 @@ def get_git_sha():
                    'rev-parse',
                    'HEAD']
 
-    return subprocess.check_output(git_command).rstrip()
+    return subprocess.check_output(
+        git_command,
+        universal_newlines=True).rstrip()
 
 
 def get_git_branch():
@@ -69,7 +71,9 @@ def get_git_branch():
                    '--abbrev-ref',
                    'HEAD']
 
-    return subprocess.check_output(git_command).rstrip()
+    return subprocess.check_output(
+        git_command,
+        universal_newlines=True).rstrip()
 
 
 def get_git_desc():
@@ -81,7 +85,9 @@ def get_git_desc():
                    '--dirty',
                    '--abbrev=40']
 
-    return subprocess.check_output(git_command).rstrip()
+    return subprocess.check_output(
+        git_command,
+        universal_newlines=True).rstrip()
 
 
 def run_packer(mmw_config, machine_types, aws_profile):
