WikiWatershed/model-my-watershed

commit de600cbf5165cf9bf87847308de5b4c22d976f83

Validate DRB, NHD based RWD requests to be in CONUS

src/mmw/js/src/draw/views.js
@@ -1017,8 +1017,13 @@ var WatershedDelineationView = DrawToolBaseView.extend({
             contentType: 'application/json'
         };
 
-        if (!utils.withinConus(point)) {
-            deferred.reject('The area of interest must be within the Continental US.');
+        if (_.includes(['drb', 'nhd'], dataSource)
+            && !coreUtils.isInConus(point)) {
+
+            deferred.reject(
+                'The Area of Interest must be within ' +
+                'the Continental United States.'
+            );
         } else {
             this.rwdTaskModel.start(taskHelper);
         }

src/mmw/js/src/draw/utils.js
@@ -214,10 +214,6 @@ function isValidForAnalysis(shape) {
     return false;
 }
 
-function withinConus(shape) {
-    return intersect(settings.get('conus_perimeter'), shape);
-}
-
 function getPolygonFromGeoJson(geojson) {
     var polygon = null;
 
@@ -257,7 +253,6 @@ module.exports = {
     shapeBoundingBoxArea: shapeBoundingBoxArea,
     isSelfIntersecting: isSelfIntersecting,
     isValidForAnalysis: isValidForAnalysis,
-    withinConus: withinConus,
     getPolygonFromGeoJson: getPolygonFromGeoJson,
     getSquareKmBoxForPoint: getSquareKmBoxForPoint,
     loadAsyncShpFilesFromZip: loadAsyncShpFilesFromZip,

src/mmw/apps/geoprocessing_api/tests.py
@@ -1412,7 +1412,9 @@ class ExerciseModeling(TestCase):
     def send_request(self, url, data):
         client = Client()
 
-        return client.post(url, data, HTTP_AUTHORIZATION=f'Token {self.token}')
+        return client.post(url, data=json.dumps(data),
+                           content_type='application/json',
+                           HTTP_AUTHORIZATION=f'Token {self.token}')
 
     def send_gwlfe_prepare(self, data):
         return self.send_request(
@@ -1653,3 +1655,33 @@ class ExerciseModelingModifications(TestCase):
 
         # Changed indirectly
         self.assertEqual(modded_input['n24b'], 2639.642634937583)
+
+
+class ExerciseRWD(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username='user', password='user',
+                                             email='user@element84.com')
+
+        self.token = Token.objects.get(user=self.user)
+
+    def send_request(self, data):
+        client = Client()
+        url = reverse('geoprocessing_api:start_rwd')
+
+        return client.post(url, data=json.dumps(data),
+                           content_type='application/json',
+                           HTTP_AUTHORIZATION=f'Token {self.token}')
+
+    def test_outside_conus_denied(self):
+        data = {
+            "location": [  # Indore, MP, India
+                75.857727,
+                22.7196
+            ],
+            "snappingOn": True,
+            "simplify": 0.0001,
+            "dataSource": "drb"
+        }
+
+        response = self.send_request(data)
+        self.assertEqual(response.status_code, 400)

src/mmw/apps/geoprocessing_api/validation.py
@@ -3,6 +3,7 @@ from numbers import Number
 from uuid import UUID
 
 from django.conf import settings
+from django.contrib.gis.geos import Point
 
 from rest_framework.exceptions import ValidationError
 
@@ -15,6 +16,13 @@ def validate_rwd(location, data_source, snapping, simplify):
     if not check_rwd_data_source(data_source):
         error = create_invalid_rwd_data_source_error_msg(data_source)
         raise ValidationError(error)
+
+    if data_source in ['drb', 'nhd'] \
+       and not check_rwd_point_within_conus(location):
+
+        raise ValidationError(
+            '`drb` and `nhd` data sources require `location` be within '
+            'Continental United States.')
 
     if type(snapping) is not bool:
         error = create_invalid_rwd_snapping_error_msg(snapping)
@@ -29,7 +37,7 @@ def validate_rwd(location, data_source, snapping, simplify):
 
 def create_invalid_rwd_location_format_error_msg(loc):
     return (f'Invalid required `location` parameter value `{loc}`. Must be a '
-            '`[lat, lng] where `lat` and `lng` are numeric.')
+            '`[lat, lng]` where `lat` and `lng` are numeric.')
 
 
 def check_location_format(loc):
@@ -62,6 +70,14 @@ def create_invalid_rwd_simplify_param_type_error_msg(simplify):
 
 def check_rwd_simplify_param_type(simplify):
     return isinstance(simplify, Number) or simplify is False
+
+
+def check_rwd_point_within_conus(loc):
+    [lat, lng] = loc
+    point = Point(lng, lat)
+    conus = settings.PERIMETERS['CONUS']['geom']
+
+    return conus.contains(point)
 
 
 def validate_uuid(uuid):

src/mmw/apps/geoprocessing_api/views.py
@@ -2304,14 +2304,7 @@ def _parse_modeling_input(request, perimeter=None):
 
 def _parse_aoi(data, perimeter=None):
     if perimeter:
-        # If perimeter is specified, pass it to the serializer so it can
-        # validate against it. The serializer expects all input within
-        # data, so we make the incoming request's QueryDict mutable to
-        # add the perimeter, then set it back to immutable. This avoids
-        # having to make a deepcopy of the request with its large AoI json.
-        data._mutable = True
         data['perimeter'] = perimeter
-        data._mutable = False
 
     serializer = AoiSerializer(data=data)
     serializer.is_valid(raise_exception=True)

commit c5769d782dd52504b5455ca95c5f0c2d271b4e6c

Fix CONUS validation for RWD
This was broken in de600cbf

src/mmw/js/src/core/utils.js
@@ -578,6 +578,8 @@ var utils = {
                 return _.flatten(_.flatten(geom.coordinates));
             case 'Polygon':
                 return _.flatten(geom.coordinates);
+            case 'Point':
+                return geom.coordinates;
             default:
                 throw new Error('Unsupported geometry');
         }

src/mmw/js/src/draw/views.js
@@ -1035,7 +1035,7 @@ var WatershedDelineationView = DrawToolBaseView.extend({
         };
 
         if (_.includes(['drb', 'nhd'], dataSource)
-            && !coreUtils.isInConus(point)) {
+            && !coreUtils.isInConus(point.geometry)) {
 
             deferred.reject(
                 'The Area of Interest must be within ' +